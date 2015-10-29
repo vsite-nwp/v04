4ac03cecdea84810e6d90dfcdc2782c0f73a6ddb
Nenad Caklovic <nenad.caklovic@avl.com>
2015-10-29 11:27:25 +0100

    Added project files

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include "nwpwin.h"
+
+// TODO: prepare class (Static) for a ship
+
+class MainWindow : public Window
+{
+protected:
+	void OnLButtonDown(POINT p) {
+		// TODO: create ship if it doesn't exist yet
+		// TODO: change current location
+	}
+	void OnKeyUp(int vk) {
+		// TODO: mark ship (if exists) as "not moving"
+	}
+	void OnKeyDown(int vk) {
+		// TODO: if ship exists, move it depending on key and mark as "moving"
+	}
+	void OnDestroy(){
+		::PostQuitMessage(0);
+	}
+private:
+};
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
+{
+	Application app;
+	MainWindow wnd;
+	wnd.Create(NULL, WS_OVERLAPPEDWINDOW | WS_VISIBLE, _T("NWP 4"));
+	return app.Run();
+}
