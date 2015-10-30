3bbb57b6419d74ab9bb8636ee38ba3b6c91b8cf2
skulenov <skulenov@gmail.com>
2015-10-30 21:36:39 +0100

    Funkcije za provjeru i postavljanje pozicije. Funkcija za kreiranje broda. Objekti za manipulaciju(RECT,POINT).

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 //Custom defines...
 #define WD_WIDTH 400
 #define WD_HEIGHT 400
-#define SH_SIZE 10
+#define SH_SIZE 20
 
 
 class Static : public Window {
@@ -12,10 +12,17 @@ public:
 
 class MainWindow : public Window
 {
+
 protected:
 	void OnLButtonDown(POINT p) {
-		// TODO: create ship if it doesn't exist yet
-		// TODO: change current location
+
+		CheckNSetPosition(p);
+
+		if (!ship) {
+			BuildShip(position);
+		}
+		
+		SetWindowPos(ship, HWND_TOP, position.x, position.y, NULL, NULL, SWP_NOSIZE | SWP_NOZORDER);
 	}
 	void OnKeyUp(int vk) {
 		// TODO: mark ship (if exists) as "not moving"
@@ -27,6 +34,38 @@ protected:
 		::PostQuitMessage(0);
 	}
 private:
+	Static ship;
+	POINT position;
+	RECT clientSize, perimeter;
+	
+
+	void BuildShip(POINT& p) {
+		ship.Create(*this, WS_CHILD | WS_VISIBLE | SS_CENTER, "X", NULL, p.x, p.y, SH_SIZE, SH_SIZE);
+	}
+
+	void CheckNSetPosition(POINT& p) {
+
+		GetClientRect(*this, &clientSize);
+
+		perimeter.bottom = clientSize.bottom - SH_SIZE;
+		perimeter.right = clientSize.right - SH_SIZE;
+		perimeter.top = clientSize.top;
+		perimeter.left = clientSize.left;
+
+		if (p.x > perimeter.right) {
+			position.x = perimeter.right;
+		}
+		else{ 
+			position.x = p.x; 
+		}
+		
+		if (p.y > perimeter.bottom) {
+			position.y = perimeter.bottom;
+		}
+		else {
+			position.y = p.y;
+		}
+	}
 };
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
