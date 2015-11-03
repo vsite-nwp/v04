7c37635110fcd46657204fa8611c6255331e02ed
Filip Horvat <fihorvat@vsite.hr>
2015-11-03 14:12:11 +0100

    ship is finally moving

--- main.cpp
+++ main.cpp
@@ -15,25 +15,55 @@ protected:
 	void OnLButtonDown(POINT p) {
 		// TODO: create ship if it doesn't exist yet
 		if (!ship){
-			ship.Create(*this, WS_CHILD | WS_VISIBLE , "X", 0, (int)p.x, (int)p.y, 20, 20);
+			ship.Create(*this, WS_CHILD | WS_VISIBLE, "X", 0, (int)p.x, (int)p.y, 15, 15);
+			cords = p;
 		}
 		// TODO: change current location
 	
-		SetWindowPos(ship, 0, (int)p.x, (int)p.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+		SetWindowPos(ship, 0, (int)p.x, (int)p.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SS_CENTER);
 	}
 	void OnKeyUp(int vk) {
 		// TODO: mark ship (if exists) as "not moving"
+	/*	if (ship){
+			SetWindowLong(ship, GWL_STYLE, ~WS_BORDER);
+		}*/
 	}
 	void OnKeyDown(int vk) {
 		// TODO: if ship exists, move it depending on key and mark as "moving"
+		if (ship){
+
+			GetClientRect(*this, &rec);
+			
+			switch (vk){
+				case VK_UP:
+					cords.y -= 5;
+					SetWindowPos(ship, 0, cords.x, rec.top < cords.y ? cords.y : rec.top, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SS_CENTER );
+						break;
+				case VK_DOWN:
+					cords.y += 5;
+					SetWindowPos(ship, 0, cords.x, rec.bottom > cords.y ? cords.y : rec.bottom, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SS_CENTER );
+					break;
+				case VK_LEFT:
+					cords.x -= 5;
+					SetWindowPos(ship, 0, rec.left < cords.x ? cords.x : rec.left, cords.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SS_CENTER );
+					break;
+				case VK_RIGHT:
+					cords.x += 5;
+					SetWindowPos(ship, 0, rec.right > cords.x ? cords.x : rec.right, cords.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SS_CENTER );
+					break;
+				default:
+					break;
+			}
+
+		}
 	}
 	void OnDestroy(){
 		::PostQuitMessage(0);
 	}
 private:
 	Static ship;
-	POINT current_position;
-
+	POINT cords;
+	RECT rec;
 
 };
 
