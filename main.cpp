528a420a8b3166084bc1db863f53721b7c57ec3f
Filip Horvat <fihorvat@vsite.hr>
2015-11-03 14:23:24 +0100

    GoShipGo

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ protected:
 			cords = p;
 		}
 		// TODO: change current location
-	
+
 		SetWindowPos(ship, 0, (int)p.x, (int)p.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SS_CENTER);
 	}
 	void OnKeyUp(int vk) {
@@ -37,21 +37,23 @@ protected:
 			switch (vk){
 				case VK_UP:
 					cords.y -= 5;
-					SetWindowPos(ship, 0, cords.x, rec.top < cords.y ? cords.y : rec.top, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SS_CENTER );
-						break;
+					rec.top < cords.y ? cords.y : cords.y = rec.top;
+					GoShipGo();
+					break;
 				case VK_DOWN:
 					cords.y += 5;
-					SetWindowPos(ship, 0, cords.x, rec.bottom > cords.y ? cords.y : rec.bottom, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SS_CENTER );
+					rec.bottom > cords.y ? cords.y : cords.y = rec.bottom;
+					GoShipGo();
 					break;
 				case VK_LEFT:
 					cords.x -= 5;
-					SetWindowPos(ship, 0, rec.left < cords.x ? cords.x : rec.left, cords.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SS_CENTER );
+					rec.left < cords.x ? cords.x : cords.x = rec.left;
+					GoShipGo();
 					break;
 				case VK_RIGHT:
 					cords.x += 5;
-					SetWindowPos(ship, 0, rec.right > cords.x ? cords.x : rec.right, cords.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SS_CENTER );
-					break;
-				default:
+					rec.right > cords.x ? cords.x : cords.x = rec.right;
+					GoShipGo();
 					break;
 			}
 
@@ -65,6 +67,11 @@ private:
 	POINT cords;
 	RECT rec;
 
+	void GoShipGo(){
+		
+		SetWindowLong(ship, GWL_STYLE, WS_BORDER);
+		::SetWindowPos(ship, 0, cords.x , cords.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SS_CENTER);
+	}
 };
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
