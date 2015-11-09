3d58f654d4ae6446edb304dc0aa9e90a0eeb7046
gajotres100 <pongo666>
2015-11-09 13:44:32 +0100

    OnButonDown

--- main.cpp
+++ main.cpp
@@ -1,14 +1,30 @@
 #include "nwpwin.h"
+#define SHIP_ID 1
 
 // TODO: prepare class (Static) for a ship
 
+class Static : public Window
+{
+	std::string ClassName(){ return "static"; }
+};
+
 class MainWindow : public Window
 {
+public:
+	Static s;
+	POINT cp;
+	RECT rect;
+	bool ctrlPritisnut = false;
 protected:
 	void OnLButtonDown(POINT p) {
-
-		// TODO: create ship if it doesn't exist yet
-		// TODO: change current location
+		if (!s)
+		{
+			GetClientRect(*this, &rect);
+			s.Create(*this, WS_CHILD | WS_VISIBLE, "X", SHIP_ID, p.x, p.y, 15, 20);
+			rect.bottom -= 20;
+			rect.right -= 15;
+			cp = p;
+		}
 	}
 	void OnKeyUp(int vk) {
 		// TODO: mark ship (if exists) as "not moving"
@@ -29,5 +45,3 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 	wnd.Create(NULL, WS_OVERLAPPEDWINDOW | WS_VISIBLE, "NWP 4");
 	return app.Run();
 }
-
-
