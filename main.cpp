6a48ffd5a9b2750fafab1ad6c0a85171eefef3ac
kaCulek <karlo2316@gmail.com>
2015-11-16 15:22:44 +0100

    Ship moving...

--- main.cpp
+++ main.cpp
@@ -14,16 +14,41 @@ protected:
 		position = p;
 		if (!ship)
 			ship.Create(*this, WS_CHILD | WS_VISIBLE | WS_BORDER, "X", NULL, position.x, position.y, 15, 20);
-		// TODO: change current location
 		SetWindowPos(ship, NULL, p.x, p.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
 		position = p;
 	}
 	void OnKeyUp(int vk) {
-		SetWindowLong(ship, GWL_STYLE, WS_CHILD | WS_VISIBLE);
-		SetWindowPos(ship, NULL, position.x, position.y, 0, 0, SWP_NOSIZE | SWP_FRAMECHANGED | SWP_NOMOVE | SWP_NOZORDER);
+		if (ship){
+			SetWindowLong(ship, GWL_STYLE, WS_CHILD | WS_VISIBLE);
+			SetWindowPos(ship, NULL, position.x, position.y, 0, 0, SWP_NOSIZE | SWP_FRAMECHANGED | SWP_NOMOVE | SWP_NOZORDER);
+		}
 	}
 	void OnKeyDown(int vk) {
 		// TODO: if ship exists, move it depending on key and mark as "moving"
+		int gasPedal = 10;
+		if (GetKeyState(VK_CONTROL) < 0)
+			gasPedal = 50;
+		RECT rekt;
+		GetClientRect(*this, &rekt);
+		SetWindowLong(ship, GWL_STYLE, WS_CHILD | WS_VISIBLE);
+		switch (vk){
+			case VK_UP:
+				position.y = max(rekt.top, position.y - gasPedal);
+				break;
+			case VK_DOWN:
+				position.y = min(rekt.bottom - 15, position.y + gasPedal);
+				break;
+			case VK_LEFT:
+				position.x = max(rekt.left, position.x - gasPedal);
+				break;
+			case VK_RIGHT:
+				position.x = min(rekt.right - 10 , position.x + gasPedal);
+				break;
+			default:
+				return;
+		}
+		SetWindowPos(ship, 0, position.x, position.y, 0, 0, SWP_NOSIZE | SWP_FRAMECHANGED | SWP_NOZORDER);
+		OnKeyUp(vk);
 	}
 	void OnDestroy(){
 		::PostQuitMessage(0);
