5256df8f11772e1d83562006ecefd6b72971b54d
Marijo Horvat <Marijo.Horvat@vsite.hr>
2016-11-07 15:59:37 +0100

    made some #defines

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "nwpwin.h"
 
-#define S_SIZE = 20
+#define SHIP_SIZE 15
+#define SLOW 5
+#define FAST 20
 
  class Static : public Window {
  public:
@@ -12,13 +14,12 @@ class MainWindow : public Window
 protected:
 	void OnLButtonDown(POINT p) { 
 		if (!ship)
-			ship.Create(*this, WS_CHILD | WS_VISIBLE |SS_CENTER, "X", NULL, p.x, p.y, 15, 15);
+			ship.Create(*this, WS_CHILD | WS_VISIBLE |SS_CENTER, "X", NULL, p.x, p.y, SHIP_SIZE, SHIP_SIZE);
 		SetWindowPos(ship, 0, p.x, p.y, 0, 0, SS_CENTER);
 		point = p;
 	}	
 
 	void OnKeyUp(int vk) {
-		// TODO: mark ship (if exists) as "not moving"
 		if (ship) {
 			SetWindowLong(ship, GWL_STYLE, WS_CHILD | WS_VISIBLE);
 			SetWindowPos(ship, NULL, point.x, point.y, 0, 0, SWP_NOSIZE | SWP_FRAMECHANGED | SWP_NOMOVE | SWP_NOZORDER);
@@ -29,41 +30,42 @@ protected:
 		GetClientRect(*this, &frame);
 
 		if (ship) {
+
 			switch (vk) {
 			case VK_UP:
 				if (GetKeyState(VK_CONTROL) < 0)
-					point.y -= 20;
+					point.y -= FAST;
 				else
-				point.y -= 5;
+					point.y -= SLOW;
 				if (point.y < frame.top)
 					point.y = frame.top;
 				SetWindowPos(ship, 0, point.x, point.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
 				break;
 			case VK_DOWN:
 				if (GetKeyState(VK_CONTROL) < 0)
-					point.y += 20;
+					point.y += FAST;
 				else
-				point.y += 5;
-				if (point.y > frame.bottom - 15)
-					point.y = frame.bottom - 15;
+					point.y += SLOW;
+				if (point.y > frame.bottom - SHIP_SIZE)
+					point.y = frame.bottom - SHIP_SIZE;
 				SetWindowPos(ship, 0, point.x, point.y, 0, 0,  SWP_NOSIZE | SWP_NOZORDER);
 				break;
 			case VK_LEFT:
 				if (GetKeyState(VK_CONTROL) < 0)
-					point.x -= 20;
+					point.x -= FAST;
 				else
-				point.x -= 5;
+					point.x -= SLOW;
 				if (point.x < frame.left)
 					point.x = frame.left;
 				SetWindowPos(ship, 0, point.x, point.y, 0, 0,  SWP_NOSIZE | SWP_NOZORDER);
 				break;
 			case VK_RIGHT:
 				if (GetKeyState(VK_CONTROL) < 0)
-					point.x += 20;
+					point.x += FAST;
 				else
-				point.x += 5;
-				if (point.x > frame.right - 15)
-					point.x = frame.right - 15;
+					point.x += SLOW;
+				if (point.x > frame.right - SHIP_SIZE)
+					point.x = frame.right - SHIP_SIZE;
 				SetWindowPos(ship, 0, point.x, point.y, 0, 0,  SWP_NOSIZE | SWP_NOZORDER);
 				break;
 			default:
@@ -81,6 +83,7 @@ private:
 	Static ship;
 	POINT point;
 	void Move(POINT p) { SetWindowPos(ship, 0, p.x, p.y, NULL, NULL,NULL); }
+
 };
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
