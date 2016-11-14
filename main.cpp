b5f5331ebf859a6e1fad8a2b51be4916c860f002
KapustaB <kapusta.borna.s@gmail.com>
2016-11-14 17:43:57 +0100

    gotov

--- main.cpp
+++ main.cpp
@@ -1,24 +1,59 @@
 #include "nwpwin.h"
+#define SHIP_MOVE 5
+#define SHIP_FAST_MOVE 20
+#define SHIP_SIZE 20
 
-// TODO: prepare class (Static) for a ship
+class Static :public Window {
+public:
+	std::string ClassName() override { return "Static"; }
+};
 
 class MainWindow : public Window
 {
 protected:
 	void OnLButtonDown(POINT p) {
-		// TODO: create ship if it doesn't exist yet
-		// TODO: change current location
+		if (!ship) ship.Create(*this, WS_CHILD | WS_VISIBLE | SS_CENTER, "x", 0, 0, 0, SHIP_SIZE, SHIP_SIZE);
+		this->p = p;
+		Move();
 	}
 	void OnKeyUp(int vk) {
-		// TODO: mark ship (if exists) as "not moving"
+		if (!ship) return;
+		Move();
 	}
 	void OnKeyDown(int vk) {
-		// TODO: if ship exists, move it depending on key and mark as "moving"
+		if (!ship) return;
+
+		RECT rect;
+		::GetClientRect(*this, &rect);
+		step = GetKeyState(VK_CONTROL) < 0 ? SHIP_MOVE : SHIP_FAST_MOVE;
+		switch (vk) {
+		case VK_UP:
+			p.y = max(0, p.y - step);
+			break;
+		case VK_DOWN:
+			p.y = min(rect.bottom - SHIP_SIZE, p.y + step);
+			break;
+		case VK_LEFT:
+			p.x = max(0, p.x - step);
+			break;
+		case VK_RIGHT:
+			p.x = min(rect.right - SHIP_SIZE, p.x + step);
+			break;
+		default:
+			break;
+		}
 	}
-	void OnDestroy(){
+	void OnDestroy() {
 		::PostQuitMessage(0);
 	}
 private:
+	Static ship;
+	POINT p;
+	int step;
+	void Move() {
+		SetWindowLong(ship, GWL_STYLE, WS_CHILD | WS_VISIBLE | SS_CENTER);
+		SetWindowPos(ship, 0, p.x, p.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+	}
 };
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
