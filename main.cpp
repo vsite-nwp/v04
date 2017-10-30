2d8a871069269843e4d8fd7a04ab98b81a6061ca
Matija Horvat <horvat2112@gmail.com>
2017-10-30 22:14:52 +0100

    gotova STATIC klasa TODO movement

--- main.cpp
+++ main.cpp
@@ -1,19 +1,41 @@
 #include "nwpwin.h"
 
 // TODO: prepare class (Static) for a ship
+class Static:public Window {
+protected:
+	std::string ClassName()override { return "Static"; }
+};
 
 class MainWindow : public Window
 {
+	Static shp;
 protected:
 	void OnLButtonDown(POINT p) {
-		// TODO: create ship if it doesn't exist yet
-		// TODO: change current location
+		if (!shp)
+			shp.Create(*this, WS_CHILD | WS_VISIBLE | SS_CENTER, "x", 0, p.x, p.y, 20, 20);
+		SetWindowPos(shp, 0, p.x, p.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
 	}
 	void OnKeyUp(int vk) {
 		// TODO: mark ship (if exists) as "not moving"
 	}
 	void OnKeyDown(int vk) {
 		// TODO: if ship exists, move it depending on key and mark as "moving"
+		if (!shp) {
+			MessageBox(*this, "Ship isn't set", "Not set", 0);
+			return;
+		}
+
+		switch (vk) {
+		case VK_LEFT:
+
+			break;
+		case VK_RIGHT:
+			break;
+		case VK_DOWN:
+			break;
+		case VK_UP:
+			
+		}
 	}
 	void OnDestroy(){
 		::PostQuitMessage(0);
