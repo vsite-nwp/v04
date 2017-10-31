fe1a8d689294cf5e4a1154a6122469a5dc22a51b
Luka N Sever <lukasever.56@gmail.com>
2017-10-31 23:39:04 +0100

    A start

--- main.cpp
+++ main.cpp
@@ -2,19 +2,44 @@
 
 // TODO: prepare class (Static) for a ship
 
+class Static : public Window {
+	std::string ClassName() override { return "STATIC"; }
+};
+
 class MainWindow : public Window
 {
+	Static st;
+	POINT currpos;
 protected:
 	void OnLButtonDown(POINT p) {
+		if (!st)
+			st.Create(*this, WS_CHILD|WS_VISIBLE|SS_CENTER, "X", 0, p.x, p.y, 20, 20);
+		SetWindowPos(st, 0, p.x, p.y, 0, 0, SWP_NOSIZE|SWP_NOZORDER);
+		currpos = p;
 		// TODO: create ship if it doesn't exist yet
 		// TODO: change current location
 	}
 	void OnKeyUp(int vk) {
 		// TODO: mark ship (if exists) as "not moving"
+		vk = WM_KEYUP;
+		while (!vk) {
+			if (st)
+				SetWindowPos(st, 0, currpos.x, currpos.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+		}
+			//SetWindowPos(st, 0, currpos.x, currpos.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
 	}
 	void OnKeyDown(int vk) {
 		// TODO: if ship exists, move it depending on key and mark as "moving"
-	}
+		switch (vk) {
+		case VK_LEFT: SetWindowPos(st, 0, 66 + currpos.x, 66 + currpos.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+		}
+		/*vk = WM_KEYDOWN;
+		if (GetAsyncKeyState(VK_LEFT)) {
+			while (!vk) {
+				SetWindowPos(st, 0, 2 + currpos.x, 2 + currpos.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+			}
+		}*/
+	} 
 	void OnDestroy(){
 		::PostQuitMessage(0);
 	}
