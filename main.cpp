ac3f66de1ff951cd1d982b6b91137a64946ee939
Matija Horvat <horvat2112@gmail.com>
2017-11-04 11:56:00 +0100

    backup

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include "nwpwin.h"
 
-class Static:public Window {
+class Static :public Window {
 protected:
 	std::string ClassName()override { return "Static"; }
 };
@@ -15,6 +15,7 @@ protected:
 			shp.Create(*this, WS_CHILD | WS_VISIBLE | SS_CENTER, "x", 0, p.x, p.y, 20, 20);
 		cords.x = p.x;
 		cords.y = p.y;
+
 		SetWindowPos(shp, 0, p.x, p.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
 	}
 	void OnKeyUp(int vk) {
@@ -23,7 +24,7 @@ protected:
 			MessageBox(*this, "Ship isn't set", "Not set", MB_ICONWARNING);
 			return;
 		}
-		
+
 
 	}
 	void OnKeyDown(int vk) {
@@ -32,21 +33,52 @@ protected:
 			return;
 		}
 
+		RECT wnd;
+		GetClientRect(*this, &wnd);
+		int mov = GetAsyncKeyState(VK_CONTROL)?20:1;
+
 		switch (vk) {
 		case VK_LEFT:
-			SetWindowPos(shp, 0, GetAsyncKeyState(VK_CONTROL)?cords.x-=20:--cords.x, cords.y, 20,20,SWP_FRAMECHANGED);
+			/*if (cords.x == 0)
+				break;*/
+			if (cords.x < 20)
+				mov = cords.x;
+
+			SetWindowPos(shp, 0, cords.x-=mov, cords.y, 20, 20, SWP_FRAMECHANGED);
 			break;
-		case VK_RIGHT:
-			SetWindowPos(shp, 0, GetAsyncKeyState(VK_CONTROL) ? cords.x += 20 : ++cords.x, cords.y, 20, 20, SWP_FRAMECHANGED);
+		case VK_RIGHT: {
+			int dif = wnd.right - cords.x-20;			//razlika u sirini shipa(20)
+			if (!dif)
+				break;
+			else if (dif < 20)
+				mov = dif;
+
+			SetWindowPos(shp, 0, cords.x+=mov, cords.y, 20, 20, SWP_FRAMECHANGED);
 			break;
-		case VK_DOWN:
-			SetWindowPos(shp, 0, cords.x, GetAsyncKeyState(VK_CONTROL) ? cords.y += 20 : ++cords.y, 20, 20, SWP_FRAMECHANGED);
+		}
+		case VK_DOWN: {
+			int dif = wnd.bottom - cords.y-20;					//razlika u visini shipa(20)
+			if (!dif) {
+				if (dif < 0)
+					SetWindowPos(shp, 0,cords.x, cords.y = wnd.bottom - 20, 20, 20, 0);
+				break;
+			}
+			else if (dif < 20)
+				mov = dif;
+
+			SetWindowPos(shp, 0, cords.x, cords.y+=mov, 20, 20, SWP_FRAMECHANGED);
 			break;
+		}
 		case VK_UP:
-			SetWindowPos(shp, 0, cords.x, GetAsyncKeyState(VK_CONTROL) ? cords.y -= 20 : --cords.y, 20, 20, SWP_FRAMECHANGED);
+			if (cords.y == 0)
+				break;
+			else if (cords.y < 20)
+				mov = cords.y;
+
+			SetWindowPos(shp, 0, cords.x, cords.y-=mov, 20, 20, SWP_FRAMECHANGED);
 		}
 	}
-	void OnDestroy(){
+	void OnDestroy() {
 		::PostQuitMessage(0);
 	}
 private:
