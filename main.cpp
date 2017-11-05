bbf7ab5871bad198656d576e0524107dc36e57a7
Matija Horvat <horvat2112@gmail.com>
2017-11-05 21:33:28 +0100

    min i max

--- main.cpp
+++ main.cpp
@@ -39,40 +39,18 @@ protected:
 
 		switch (vk) {
 		case VK_LEFT:
-			if (!cords.x)
-				break;
-			else if (cords.x < 20)
-				mov = cords.x;
-
-			cords.x -= mov;
+			cords.x = max(cords.x - mov, 0);
 			break;
 		case VK_RIGHT: {
-			int dif = wnd.right-cords.x-20;
-			if (!dif)
-				break;
-			else if (dif<20&&mov==20)
-				mov = dif;
-			
-			cords.x += mov;
+			cords.x = min(cords.x + mov, wnd.right-20);
 			break;
 		}
 		case VK_DOWN: {
-			int dif = wnd.bottom - cords.y-20;
-			if (!dif)
-				break;
-			else if (dif < 20 && mov == 20)
-				mov = dif;
-
-			cords.y += mov;
+			cords.y = min(cords.y + mov, wnd.bottom - 20);
 			break;
 		}
 		case VK_UP:
-			if (!cords.y)
-				break;
-			else if (cords.y < 20)
-				mov = cords.y;
-
-			cords.y -= mov;
+			cords.y = max(cords.y - mov, 0);
 			break;
 		}
 
