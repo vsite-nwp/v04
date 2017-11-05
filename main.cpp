7453eeb811dc588b93c15bd180fe121cecf25bd6
Matija Horvat <horvat2112@gmail.com>
2017-11-05 18:27:36 +0100

    obrub TODO izracun u caseu

--- main.cpp
+++ main.cpp
@@ -19,15 +19,17 @@ protected:
 		SetWindowPos(shp, 0, p.x, p.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
 	}
 	void OnKeyUp(int vk) {
-		// TODO: mark ship (if exists) as "not moving"
 		if (!shp) {
 			MessageBox(*this, "Ship isn't set", "Not set", MB_ICONWARNING);
 			return;
 		}
 
-
+		SetWindowLong(shp, GWL_STYLE, WS_VISIBLE | SS_CENTER | WS_CHILD);
+		SetWindowPos(shp, 0, cords.x, cords.y, 20, 20, SWP_FRAMECHANGED | SWP_NOSIZE | SWP_NOZORDER);
 	}
 	void OnKeyDown(int vk) {
+		//TODO: napisati izracun u caseu, pomak iza casea(i promjena okvira)
+
 		if (!shp) {
 			MessageBox(*this, "Ship isn't set", "Not set", MB_ICONWARNING);
 			return;
@@ -44,7 +46,8 @@ protected:
 			else if (cords.x < 20)
 				mov = cords.x;
 
-			SetWindowPos(shp, 0,cords.x-=mov, cords.y, 20,20,SWP_FRAMECHANGED);
+			SetWindowLong(shp, GWL_STYLE, WS_BORDER | WS_VISIBLE | SS_CENTER | WS_CHILD);
+			SetWindowPos(shp, 0,cords.x-=mov, cords.y, 20,20,SWP_FRAMECHANGED|SWP_NOSIZE|SWP_NOZORDER);
 			break;
 		case VK_RIGHT: {
 			int dif = wnd.right-cords.x-20;
@@ -53,7 +56,8 @@ protected:
 			else if (dif<20&&mov==20)
 				mov = dif;
 
-			SetWindowPos(shp, 0, cords.x += mov, cords.y, 20, 20, SWP_FRAMECHANGED);
+			SetWindowLong(shp, GWL_STYLE, WS_BORDER | WS_VISIBLE | SS_CENTER | WS_CHILD);
+			SetWindowPos(shp, 0, cords.x += mov, cords.y, 20, 20, SWP_FRAMECHANGED | SWP_NOSIZE | SWP_NOZORDER);
 			break;
 		}
 		case VK_DOWN: {
@@ -63,7 +67,8 @@ protected:
 			else if (dif < 20 && mov == 20)
 				mov = dif;
 
-			SetWindowPos(shp, 0, cords.x, cords.y += mov, 20, 20, SWP_FRAMECHANGED);
+			SetWindowLong(shp, GWL_STYLE, WS_BORDER | WS_VISIBLE | SS_CENTER | WS_CHILD);
+			SetWindowPos(shp, 0, cords.x, cords.y += mov, 20, 20, SWP_FRAMECHANGED | SWP_NOSIZE | SWP_NOZORDER);
 			break;
 		}
 		case VK_UP:
@@ -72,7 +77,8 @@ protected:
 			else if (cords.y < 20)
 				mov = cords.y;
 
-			SetWindowPos(shp, 0, cords.x, cords.y-=mov, 20, 20, SWP_FRAMECHANGED);
+			SetWindowLong(shp, GWL_STYLE, WS_BORDER | WS_VISIBLE | SS_CENTER | WS_CHILD);
+			SetWindowPos(shp, 0, cords.x, cords.y-=mov, 20, 20, SWP_FRAMECHANGED | SWP_NOSIZE | SWP_NOZORDER);
 		}
 	}
 	void OnDestroy() {
