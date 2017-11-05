2496e04ffa4665f809b31528b6877713ae152fb7
Dijana Molnar <dijanamolnar89@gmail.com>
2017-11-05 14:16:12 +0100

    Gotov zadatak

--- main.cpp
+++ main.cpp
@@ -9,19 +9,51 @@ class MainWindow : public Window
 protected:
 	void OnLButtonDown(POINT p) {
 		if (!ship)
-			ship.Create(*this, WS_CHILD|WS_VISIBLE|SS_CENTER, "X", 0, p.x, p.y, 20, 20);
-		SetWindowPos(ship, 0, p.x, p.y, 0, 0, SWP_NOSIZE|SWP_NOZORDER|SS_CENTER);
+			ship.Create(*this, WS_CHILD | WS_VISIBLE | SS_CENTER, "X", 0, p.x, p.y, 20, 20);
+		SetWindowPos(ship, 0, p.x, p.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SS_CENTER);
 		currentP = p;
 	}
 	void OnKeyUp(int vk) {
 		if (ship) {
-			SetWindowLong(ship, GWL_STYLE, WS_CHILD|WS_VISIBLE|SS_CENTER);
-			SetWindowPos(ship, 0, 0, 0, 0, 0, SWP_NOSIZE|SWP_NOZORDER|SWP_NOMOVE|SWP_FRAMECHANGED);
+			SetWindowLong(ship, GWL_STYLE, WS_CHILD | WS_VISIBLE | SS_CENTER);
+			SetWindowPos(ship, 0, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_NOMOVE | SWP_FRAMECHANGED);
 		}
 	}
 	void OnKeyDown(int vk) {
-		// TODO: if ship exists, move it depending on key and mark as "moving"
-		// VK_LEFT, VK_RIGHT, VK_UP, VK_DOWN
+		if (ship) {
+			RECT glavniP;
+			GetClientRect(*this, &glavniP);
+			int brzina = 2;
+			if (GetKeyState(VK_CONTROL) < 0)
+				brzina = 10;
+			switch (vk)
+			{
+			case VK_LEFT:
+				currentP.x -= brzina;
+				if (currentP.x < glavniP.left)
+					currentP.x = glavniP.left;
+				break;
+			case VK_RIGHT:
+				currentP.x += brzina;
+				if (currentP.x > glavniP.right - 20)
+					currentP.x = glavniP.right - 20;
+				break;
+			case VK_UP:
+				currentP.y -= brzina;
+				if (currentP.y < glavniP.top)
+					currentP.y = glavniP.top;
+				break;
+			case VK_DOWN:
+				currentP.y += brzina;
+				if (currentP.y > glavniP.bottom - 20)
+					currentP.y = glavniP.bottom - 20;
+				break;
+			default:
+				return;
+			}
+			SetWindowLong(ship, GWL_STYLE, WS_CHILD | WS_VISIBLE | SS_CENTER | WS_BORDER);
+			SetWindowPos(ship, 0, currentP.x, currentP.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
+		}
 	}
 	void OnDestroy(){
 		::PostQuitMessage(0);
