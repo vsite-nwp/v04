1e82b626f419c76c2848af5bda630e344dc620a4
Dijana Molnar <dijanamolnar89@gmail.com>
2017-11-05 11:02:53 +0100

    Pripremljena klasa, napravljeno OnLButtonDown

--- main.cpp
+++ main.cpp
@@ -1,24 +1,31 @@
 #include "nwpwin.h"
 
-// TODO: prepare class (Static) for a ship
+class Static : public Window {
+	std::string ClassName() override { return "Static"; };
+};
 
 class MainWindow : public Window
 {
 protected:
 	void OnLButtonDown(POINT p) {
-		// TODO: create ship if it doesn't exist yet
-		// TODO: change current location
+		if (!ship)
+			ship.Create(*this,WS_CHILD|WS_VISIBLE|SS_CENTER,"X",p.x, p.y,20,20);
+		SetWindowPos(ship, 0, p.x, p.y, 0, 0, SWP_NOSIZE|SWP_NOZORDER | SS_CENTER);
+		currentP = p;
 	}
 	void OnKeyUp(int vk) {
 		// TODO: mark ship (if exists) as "not moving"
 	}
 	void OnKeyDown(int vk) {
 		// TODO: if ship exists, move it depending on key and mark as "moving"
+		// VK_LEFT, VK_RIGHT, VK_UP, VK_DOWN
 	}
 	void OnDestroy(){
 		::PostQuitMessage(0);
 	}
 private:
+	Static ship;
+	POINT currentP;
 };
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
