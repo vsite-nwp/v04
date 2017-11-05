f45fc10a1f2f4f978d909808a79e6a1d3b6fdb38
Perica Jurić <pejuric@vsite.hr>
2017-11-06 00:18:07 +0100

    NWP vj4 Juric Perica

--- main.cpp
+++ main.cpp
@@ -14,61 +14,83 @@ class MainWindow : public Window
 	UINT key;
 	Static ship;
 	POINT current_pos;
-	
+	RECT sea;
 protected:
-
-	////////////////////////////////////////
-	/*void OnSubclass(HWND hWnd)
-	{
-
-		WNDPROC oldProc = reinterpret_cast<WNDPROC>(SetWindowLong(hWnd, GWL_WNDPROC, (LONG)newProc));
-
-	}
-		static LRESULT CALLBACK newProc(HWND hw, UINT msg, WPARAM wp, LPARAM lp)
-		{
-			if (msg == WM_CHAR)
-				return 0;
-			return CallWindowProc(oldProc, hw, msg, wp, lp);
-		}*/
-	/////////////////////////////////////////////////////
 
 	void OnLButtonDown(POINT p) {
 		if (!ship)
-			ship.Create(*this, WS_CHILD | WS_VISIBLE | SS_CENTER, "x",0, p.x, p.y,20,20 );
+			ship.Create(*this, WS_CHILD | WS_VISIBLE | SS_CENTER , "x",0, p.x, p.y,20,20 );
 		SetWindowPos(ship, 0, p.x, p.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
-		current_pos = p;
-	
+		current_pos = p;	
 	}
-	void OnKeyUp(int vk) {
-		
-		// TODO: mark ship (if exists) as "not moving"
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	void OnKeyUp(int vk) 
+	{
+		not_moving();
 	}
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 	void OnKeyDown(int vk) {
 		if (!ship)
-			return;
+			return;	
+
+		GetClientRect(*this, &sea);
+
 		int speed;
 		if (GetKeyState(VK_CONTROL) & 0x8000)
 			speed = 15;
 		else
 			speed = 5;
-		if (vk == VK_UP)
+	// / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /
+		if ((GetKeyState(VK_UP)&0x8000)&&(GetKeyState(VK_LEFT)&0x8000)&&current_pos.x>5&&current_pos.y>25)
+		{
 			current_pos.y -= speed;
-		if (vk == VK_DOWN)
-			current_pos.y += speed;
-		if (vk == VK_LEFT)
 			current_pos.x -= speed;
-		if (vk == VK_RIGHT)
+		}
+		else if((GetKeyState(VK_DOWN)&0x8000)&&(GetKeyState(VK_LEFT)&0x8000)&&current_pos.y<sea.bottom-25&&current_pos.x>5)
+		{
+			current_pos.y += speed;
+			current_pos.x -= speed;
+		}
+		else if((GetKeyState(VK_DOWN)&0x8000)&&(GetKeyState(VK_RIGHT)&0x8000&&current_pos.y<sea.bottom-25&&current_pos.x<sea.right-25))
+		{
+			current_pos.y += speed;
 			current_pos.x += speed;
-	
-		SetWindowPos(ship,0,current_pos.x,current_pos.y,0,0, SWP_NOSIZE|SWP_NOZORDER);
-
+		}
+		else if((GetKeyState(VK_UP)&0x8000)&&(GetKeyState(VK_RIGHT)&0x8000)&&current_pos.x<sea.right-25&&current_pos.y>25)
+		{
+			current_pos.y -= speed;
+			current_pos.x += speed;
+		}
+		else if (vk==VK_UP&&current_pos.y>25)
+			current_pos.y -= speed;
+		else if (vk==VK_DOWN&&current_pos.y<sea.bottom-25)
+			current_pos.y += speed;
+		else if (vk==VK_LEFT&&current_pos.x>25)
+			current_pos.x -= speed;
+		else if (vk==VK_RIGHT&&current_pos.x<sea.right-25)
+			current_pos.x += speed;
 		
-		// TODO: if ship exists, move it depending on key and mark as "moving"
+		moving();	
 	}
-	void OnDestroy(){
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	void OnDestroy()
+	{
 		::PostQuitMessage(0);
 	}
 private:
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	void moving() 
+	{
+		SetWindowLong(ship,GWL_STYLE,WS_BORDER|WS_CHILD|WS_VISIBLE|SS_CENTER);
+		SetWindowPos(ship,0,current_pos.x,current_pos.y,0,0,SWP_NOSIZE|SWP_NOZORDER|SWP_FRAMECHANGED);
+	}
+	void not_moving()
+	{
+		SetWindowLong(ship, GWL_STYLE,  WS_CHILD | WS_VISIBLE | SS_CENTER);
+		SetWindowPos(ship,0,current_pos.x,current_pos.y,0,0,SWP_NOSIZE|SWP_NOZORDER|SWP_NOMOVE|SWP_FRAMECHANGED);
+	}
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 };
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
