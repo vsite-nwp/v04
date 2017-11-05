e8640f70321051c4467116d3aaa5dbc7a2054a2c
Bruno Brckovic <bruno.brckovic@gmail.com>
2017-11-06 00:53:54 +0100

    Gotovo, Din...

--- main.cpp
+++ main.cpp
@@ -9,11 +9,14 @@ class MainWindow : public Window
 	Static st;
 	POINT currPos;
 	int border;
+	int sizeX = 20;
+	int sizeY = 20;
+
 protected:
 	void OnLButtonDown(POINT p) {
 		if (!st)
-			st.Create(*this, WS_CHILD | WS_VISIBLE, ";)", NULL, p.x, p.y, 20, 20);
-		SetWindowPos(st, NULL, p.x, p.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+			st.Create(*this, WS_CHILD | WS_VISIBLE, ";)", NULL, p.x, p.y, sizeX, sizeY);
+		SetWindowPos(st, NULL, p.x, p.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SS_CENTER);
 		currPos = p;
 	};
 
@@ -25,22 +28,25 @@ protected:
 	void OnKeyDown(int vk) {
 		if (st)
 		{
-			int Speed = 4;
+			int Speed = 5;
 			if (GetKeyState(VK_LCONTROL) == -128)
 				Speed = 20;
+
+			RECT stPosition;
+			GetClientRect(*this, &stPosition);
 
 			switch (vk) {
 				case (VK_LEFT) :
 					currPos.x = max(0, currPos.x - Speed);
 					break;
 				case (VK_RIGHT) :
-					currPos.x = max(0 ,currPos.x + Speed);
+					currPos.x = min(stPosition.right - sizeX, currPos.x + Speed);
 					break;
 				case (VK_UP) :
 					currPos.y = max(0, currPos.y - Speed);
 					break;
 				case(VK_DOWN) :
-					currPos.y = max(0, currPos.y + Speed);
+					currPos.y = min(stPosition.bottom - sizeY , currPos.y + Speed);
 					break;
 				default:
 					return;
