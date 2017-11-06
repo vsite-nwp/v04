75c2ca0ae24ed2e5b34f4892a1ca4203d5c66b8b
Petar Domitrović <domitrovicpetar@gmail.com>
2017-11-06 16:10:53 +0100

    Zadatak riješen. Posotje neki čudni problemi kada se koristi CTRL za veći korak  kod kraja prozora.

--- main.cpp
+++ main.cpp
@@ -1,24 +1,79 @@
 #include "nwpwin.h"
 
-// TODO: prepare class (Static) for a ship
+class Static : public Window{
+public:
+	std::string ClassName() override { return "STATIC"; }
+};
 
 class MainWindow : public Window
 {
+	Static st;
+	POINT current_pos;
 protected:
 	void OnLButtonDown(POINT p) {
-		// TODO: create ship if it doesn't exist yet
-		// TODO: change current location
+		if (!st)
+			st.Create(*this, WS_CHILD | WS_VISIBLE | SS_CENTER, "x", 0, p.x, p.y, 20, 20);
+		SetWindowPos(st, 0, p.x, p.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+		current_pos = p;
 	}
 	void OnKeyUp(int vk) {
-		// TODO: mark ship (if exists) as "not moving"
+		if (!st)
+			return;
+		SetWindowLongPtr(st, GWL_STYLE, WS_VISIBLE | WS_CHILD);
+		SetWindowPos(st, 0, current_pos.x, current_pos.y, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);	
 	}
 	void OnKeyDown(int vk) {
-		// TODO: if ship exists, move it depending on key and mark as "moving"
+		if (!st)
+			return;
+		RECT r, s;
+		LPRECT cr = &r;
+		LPRECT sr = &s;
+		GetClientRect(*this, cr);
+		GetClientRect(st, sr);
+		int step = 1;
+		int bend = cr->bottom - sr->bottom - 5;
+		int rend = cr->right - sr->right - 5;
+
+		if (GetKeyState(VK_CONTROL) < 0)
+			step *= 8;
+		switch (vk)
+		{
+		case VK_UP:
+			if (current_pos.y < cr->top) {
+				current_pos.y = cr->top;
+				break;
+			}
+			current_pos.y -= step;
+			break;
+		case VK_DOWN:
+			if (current_pos.y > bend) {
+				current_pos.y = bend;
+				break;
+			}
+			current_pos.y += step;
+			break;
+		case VK_LEFT:
+			if (current_pos.x < cr->left ) {
+				current_pos.x = cr->left;
+				break;
+			}
+			current_pos.x -= step;
+			break;
+		case VK_RIGHT:
+			if (current_pos.x > rend){
+				current_pos.x = rend;
+				break;
+		}
+			current_pos.x += step;
+			break;
+		}
+		SetWindowLongPtr(st, GWL_STYLE, WS_BORDER | WS_VISIBLE | WS_CHILD);
+		SetWindowPos(st, 0, current_pos.x, current_pos.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
 	}
-	void OnDestroy(){
+
+	void OnDestroy() {
 		::PostQuitMessage(0);
 	}
-private:
 };
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
