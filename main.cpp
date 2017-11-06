ea1f48ca5ce058bbf1cc04425168e38a8dd6d477
Bruno Brckovic <bruno.brckovic@gmail.com>
2017-11-06 19:07:16 +0100

    Gotova vjezba

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@ class Static : public Window{
 	std::string ClassName() { return "Static"; }
 };
 
+const int sizeX = 20;
+const int sizeY = 20;
+
 class MainWindow : public Window
 {
 	Static st;
@@ -13,16 +16,14 @@ class MainWindow : public Window
 protected:
 	void OnLButtonDown(POINT p) {
 		if (!st)
-			st.Create(*this, WS_CHILD | WS_VISIBLE, ":)", NULL, p.x, p.y, 20, 20);
+			st.Create(*this, WS_CHILD | WS_VISIBLE | SS_CENTER, ":)", NULL, p.x, p.y, sizeX, sizeY);
 		currPos = p;
-		style = GetWindowLong(st, GWL_STYLE);
-		SetWindowPos(st, NULL, currPos.x, currPos.y, 0, 0, SWP_FRAMECHANGED | SWP_NOSIZE | SWP_NOZORDER);
+		naCrtajMe(false);
 	};
 
 	void OnKeyUp(int vk) {
 		if (st){
-			SetWindowLong(st, GWL_STYLE, style);
-			SetWindowPos(st, NULL, currPos.x, currPos.y, 0, 0, SWP_FRAMECHANGED | SWP_NOSIZE | SWP_NOZORDER);
+			naCrtajMe(false); 
 		}
 	}
 	void OnKeyDown(int vk) {
@@ -40,21 +41,26 @@ protected:
 					currPos.x = max(0, currPos.x - Speed);
 					break;
 				case (VK_RIGHT) :
-					currPos.x = min(stPosition.right - 20, currPos.x + Speed);
+					currPos.x = min(stPosition.right - sizeX, currPos.x + Speed);
 					break;
 				case (VK_UP) :
 					currPos.y = max(0, currPos.y - Speed);
 					break;
 				case(VK_DOWN) :
-					currPos.y = min(stPosition.bottom - 20 , currPos.y + Speed);
+					currPos.y = min(stPosition.bottom - sizeY, currPos.y + Speed);
 					break;
 				default:
 					return;
 				}
-			SetWindowLong(st, GWL_STYLE, style | WS_BORDER);
-			SetWindowPos(st, NULL, currPos.x, currPos.y, 0, 0, SWP_FRAMECHANGED | SWP_NOSIZE | SWP_NOZORDER);
+			naCrtajMe(true);
 		}
 	}
+	void naCrtajMe(bool haveBorder){
+			DWORD current_style = GetWindowLong(st, GWL_STYLE);
+			current_style = haveBorder ? (current_style | WS_BORDER) : (current_style & ~WS_BORDER);
+			SetWindowLong(st, GWL_STYLE, current_style);
+			SetWindowPos(st, NULL, currPos.x, currPos.y, 0, 0, SWP_FRAMECHANGED | SWP_NOSIZE | SWP_NOZORDER);
+		}
 
 	void OnDestroy(){
 		::PostQuitMessage(0);
