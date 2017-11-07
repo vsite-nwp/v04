7ed533a9062742f477683f5c2403d5e885734f06
Marin Pepeljnjak <marin.pepeljnjak@vsite.hr>
2017-11-07 18:29:21 +0100

    commit mi nije radio evo sada sve

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "nwpwin.h"
 #include "math.h"
 
-// TODO: prepare class (Static) for a ship
+// prepare class (Static) for a ship
 class Static:public Window{
 	std::string ClassName() override { return "STATIC"; }
 	
@@ -20,66 +20,39 @@ protected:
 	}
 	void OnKeyUp(int vk)
 	{
-		if (st)
-		{
-			SetWindowLong(st, GWL_EXSTYLE, WS_CHILD | WS_VISIBLE | SS_CENTER);
-			SetWindowPos(st, 0, current_pos.x, current_pos.y, 0, 0, SWP_FRAMECHANGED | SWP_NOSIZE | SWP_NOZORDER);
-		}
-	}
-	void position_change(int x, int y)
-	{
-		RECT right_wall;
-		GetClientRect(*this, &right_wall);
-		if (current_pos.x < abs(x) && x < 0)
-		{
-			current_pos.x = 0;
-		}
-		else if (current_pos.x > right_wall.right - 20 - x)
-		{
-			current_pos.x = right_wall.right - 20;
-		}
-		else
-		{
-			current_pos.x += x;
-		}
-		RECT floor;
-		GetClientRect(*this, &floor);
-		if (current_pos.y < abs(y) && y < 0)
-		{
-			current_pos.y = 0;
-		}
-		else if(current_pos.y>floor.bottom-20-y)
-		{
-			current_pos.y = floor.bottom - 20;
-		}
-		else
-		{
-			current_pos.y += y;
-		}
-		SetWindowPos(st, 0, current_pos.x, current_pos.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
-		SetWindowLong(st, GWL_EXSTYLE, WS_CHILD | WS_VISIBLE | SS_CENTER | WS_BORDER);
-		SetWindowPos(st, 0, current_pos.x, current_pos.y, 0, 0, SWP_FRAMECHANGED | SWP_NOSIZE | SWP_NOZORDER);
+		Border(false);
 	}
 	void OnKeyDown(int vk) {
-		int x=0, y=0;
+		RECT wdw;
+		GetClientRect(*this, &wdw);
+		int step = GetKeyState(VK_CONTROL) & 0x8000 ? 50 : 10;
 		switch (vk) {
 		case VK_LEFT:
-			x = -10;
+			current_pos.x = max(0, current_pos.x - step);
 			break;
 		case VK_RIGHT:
-			x = 10;
+			current_pos.x = min(wdw.right-20, current_pos.x + step);
 			break;
 		case VK_DOWN:
-			y = 10;
+			current_pos.y = min(wdw.bottom - 20, current_pos.y + step);
 			break;
 		case VK_UP:
-			y = -10;
+			current_pos.y = max(0, current_pos.y - step);
 			break;
 		}
-		if (GetKeyState(VK_CONTROL) & 0x8000)
-			x *= 5, y *= 5;
-		position_change(x, y);
-		// TODO: if ship exists, move it depending on key and mark as "moving"
+		Border(true);
+		// if ship exists, move it depending on key and mark as "moving"
+	}
+	void promjena()
+	{
+		//samo da mi proradi commit
+	}
+	void Border(bool on)
+	{
+		long style = GetWindowLong(st, GWL_STYLE);
+		style = on ? style | WS_BORDER : style& ~WS_BORDER;
+		SetWindowLong(st, GWL_STYLE, style);
+		SetWindowPos(st, 0, current_pos.x, current_pos.y, 0, 0, SWP_FRAMECHANGED | SWP_NOSIZE | SWP_NOZORDER);
 	}
 	void OnDestroy(){
 		::PostQuitMessage(0);
