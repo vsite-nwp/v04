78b6c3ef5ba449f5829568b29307617911d35dc9
Andro Bošnjak <andro.bosnjak172@gmail.com>
2018-10-31 22:03:20 +0100

    Ispravljeno

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include "nwpwin.h"
-
+#define SHIP_LENGTH 20
 class Static :public Window
 {
 public:
+	bool isMoving = false;
 	std::string ClassName() override { return "STATIC"; }
+};
+
+struct Step {
+	const int slow = 1;
+	const int fast = 5;
 };
 
 class MainWindow : public Window
@@ -11,61 +17,59 @@ class MainWindow : public Window
 protected:
 	void OnLButtonDown(POINT p) {
 		if (!ship)
-			ship.Create(*this, WS_CHILD | WS_VISIBLE | SS_CENTER, "X", 0, p.x, p.y, 20, 20);
+			ship.Create(*this, WS_CHILD | WS_VISIBLE | SS_CENTER, "X", 0, p.x, p.y, SHIP_LENGTH, SHIP_LENGTH);
 		else
-			SetWindowPos(ship, *this, p.x, p.y, 20, 20, SWP_NOSIZE | SWP_NOZORDER);
+			SetWindowPos(ship, *this, p.x, p.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
 		current_position = p;
 	}
 	void OnKeyUp(int vk) {
-		if (ship) {
-			SetWindowLong(ship, GWL_STYLE, WS_CHILD | WS_VISIBLE | SS_CENTER);
-			SetWindowPos(ship, *this, current_position.x, current_position.y, 20, 20,
+		if (ship && ship.isMoving) {
+			DWORD current_style = GetWindowLong(ship, GWL_STYLE);
+			SetWindowLong(ship, GWL_STYLE, current_style ^ WS_BORDER);
+			SetWindowPos(ship, *this, current_position.x, current_position.y, 0, 0,
 				SWP_FRAMECHANGED | SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER);
+			ship.isMoving = false;
 		}
-
 	}
 	void OnKeyDown(int vk) {
 		bool ctrl = GetAsyncKeyState(VK_CONTROL);
+		RECT main_rect;
 		GetClientRect(*this, &main_rect);
+		Step step;
 		switch (vk) {
 		case VK_UP:
 			if (ctrl)
-				current_position.y -= 5;
-			current_position.y -= 1;
+				current_position.y -= step.fast;
+			current_position.y -= step.slow;
+			current_position.y = max(current_position.y, main_rect.top);
 			break;
 		case VK_DOWN:
 			if (ctrl)
-				current_position.y += 5;
-			current_position.y += 1;
+				current_position.y += step.fast;
+			current_position.y += step.slow;
+			current_position.y = min(current_position.y, main_rect.bottom - SHIP_LENGTH);
 			break;
 		case VK_LEFT:
 			if (ctrl)
-				current_position.x -= 5;
-			current_position.x -= 1;
+				current_position.x -= step.fast;
+			current_position.x -= step.slow;
+			current_position.x = max(current_position.x, main_rect.left);
 			break;
 		case VK_RIGHT:
 			if (ctrl)
-				current_position.x += 5;
-			current_position.x += 1;
+				current_position.x += step.fast;
+			current_position.x += step.slow;
+			current_position.x = min(current_position.x, main_rect.right - SHIP_LENGTH);
 			break;
 		}
 
-		if (current_position.x > main_rect.right - 20)
-			current_position.x = main_rect.right - 20;
-		else if (current_position.y > main_rect.bottom - 20)
-			current_position.y = main_rect.bottom - 20;
-		else if (current_position.x < 0)
-			current_position.x = 0;
-		else if (current_position.y < 0)
-			current_position.y = 0;
-
-		SetWindowPos(ship, *this, current_position.x, current_position.y, 20, 20,
-			SWP_NOSIZE | SWP_NOZORDER);
-		DWORD current_style = GetWindowLong(ship, GWL_STYLE);
-		SetWindowLong(ship, GWL_STYLE, WS_BORDER | current_style);
-		SetWindowPos(ship, *this, current_position.x, current_position.y, 20, 20,
-			SWP_FRAMECHANGED | SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER);
-
+		if (vk == VK_UP || vk == VK_DOWN || vk == VK_LEFT || vk == VK_RIGHT) {
+			ship.isMoving = true;
+			DWORD current_style = GetWindowLong(ship, GWL_STYLE);
+			SetWindowLong(ship, GWL_STYLE, WS_BORDER | current_style);
+			SetWindowPos(ship, *this, current_position.x, current_position.y, 0, 0,
+				SWP_FRAMECHANGED | SWP_NOSIZE | SWP_NOZORDER);
+		}
 	}
 	void OnDestroy() {
 		::PostQuitMessage(0);
@@ -73,7 +77,6 @@ protected:
 private:
 	Static ship;
 	POINT current_position;
-	RECT main_rect;
 };
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
