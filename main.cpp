6a43d49f51d9503b71698abc154a9c5867165c29
Darjan Kusan <600zgb@windowslive.com>
2018-10-31 03:43:00 +0100

    drugi pokusaj

--- main.cpp
+++ main.cpp
@@ -13,63 +13,56 @@ class MainWindow : public Window
 {
 protected:
 	void OnLButtonDown(POINT p) {
-		if(!ship)
-			ship.Create(*this, WS_CHILD | WS_VISIBLE | SS_CENTER, "X", 0 ,p.x, p.y, ship_size.cx,ship_size.cy);
+		if (!ship) 
+		{
+			ship.Create(*this, WS_CHILD | WS_VISIBLE | SS_CENTER, "X", 0, p.x, p.y, ship_size.cx, ship_size.cy);
+			style = GetWindowLong(ship, GWL_STYLE);
+		}
 		else 
 		{
-			DestroyWindow(ship);
-			ship.Create(*this, WS_CHILD | WS_VISIBLE | SS_CENTER, "X", 0, p.x, p.y, ship_size.cx, ship_size.cy);
+			SetWindowPos(ship, 0, p.x, p.y, 0, 0, SWP_NOZORDER | SWP_NOSIZE);			
 		}
+		position = {p.x,p.y};
 	}
 	void OnKeyUp(int vk) {
-		SetWindowLong(ship, GWL_STYLE, WS_CHILD | WS_VISIBLE | SS_CENTER);
-		SetWindowPos(ship, 0, 0, 0, 0, 0,
-			SWP_SHOWWINDOW | SWP_FRAMECHANGED | SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER);
+		SetWindowLong(ship, GWL_STYLE, style);
+		SetWindowPos(ship, 0, 0, 0, 0, 0, SWP_FRAMECHANGED | SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER);
 	}
 	void OnKeyDown(int vk) {
 		bool ctrl = ::GetAsyncKeyState(VK_CONTROL);
 		int speed = ctrl ? 5 : 1;
 
-
-		RECT rect_ship;		
-		GetWindowRect(ship, &rect_ship);
-		POINT position = { rect_ship.left, rect_ship.top };
-		
 		RECT rect_this;
 		GetWindowRect(*this, &rect_this);
 
 		switch (vk) 
 		{			
 		case VK_UP:
-			if(rect_this.top + 30 <position.y)
-				position.y -= speed;
+			position.y = max(rect_this.top, position.y - speed);
 			break;
 		case VK_DOWN:
-			if (rect_this.bottom - 30 > position.y)
-				position.y += speed;
+			position.y = min(rect_this.bottom - 40, position.y + speed);
 			break;
 		case VK_LEFT:
-			if(rect_this.left <position.x)
-				position.x -= speed;
+			position.x = max(rect_this.left, position.x - speed);
 			break;
 		case VK_RIGHT:
-			if (rect_this.right - 30 > position.x)
-				position.x += speed;
+			position.x = min(rect_this.right - 40, position.x + speed);
 			break;
+		default:
+			return;
 		}
-		ScreenToClient(*this, &position);
-		SetWindowLong(ship, GWL_STYLE, WS_CHILD | WS_VISIBLE | SS_CENTER | WS_BORDER);
-		SetWindowPos( ship, 0, position.x, position.y, 0, 0, 
-			SWP_SHOWWINDOW | SWP_NOZORDER | SWP_NOSIZE | SWP_FRAMECHANGED);
-		
-		
-		
+
+		SetWindowLong(ship, GWL_STYLE, style | WS_BORDER);
+		SetWindowPos( ship, 0, position.x, position.y, 0, 0, SWP_NOZORDER | SWP_NOSIZE | SWP_FRAMECHANGED);				
 	}
 	void OnDestroy(){
 		::PostQuitMessage(0);
 	}
 private:
 	Static ship;
+	POINT position;
+	DWORD style;
 };
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
