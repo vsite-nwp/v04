ef7bd492c161c081b264ae5a268a5d95b85cd3f7
Andro Bošnjak <andro.bosnjak172@gmail.com>
2018-11-02 14:03:31 +0100

    ispravljeno,za sad

--- main.cpp
+++ main.cpp
@@ -3,13 +3,7 @@
 class Static :public Window
 {
 public:
-	bool isMoving = false;
 	std::string ClassName() override { return "STATIC"; }
-};
-
-struct Step {
-	const int slow = 1;
-	const int fast = 5;
 };
 
 class MainWindow : public Window
@@ -23,53 +17,40 @@ protected:
 		current_position = p;
 	}
 	void OnKeyUp(int vk) {
-		if (ship && ship.isMoving) {
+		if (ship) {
 			DWORD current_style = GetWindowLong(ship, GWL_STYLE);
-			SetWindowLong(ship, GWL_STYLE, current_style ^ WS_BORDER);
+			SetWindowLong(ship, GWL_STYLE, current_style & ~WS_BORDER);
 			SetWindowPos(ship, *this, current_position.x, current_position.y, 0, 0,
 				SWP_FRAMECHANGED | SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER);
-			ship.isMoving = false;
 		}
 	}
 	void OnKeyDown(int vk) {
 		bool ctrl = GetAsyncKeyState(VK_CONTROL);
 		RECT main_rect;
 		GetClientRect(*this, &main_rect);
-		Step step;
+		const int step = ctrl ? 5 : 1;
 		switch (vk) {
 		case VK_UP:
-			if (ctrl)
-				current_position.y -= step.fast;
-			current_position.y -= step.slow;
-			current_position.y = max(current_position.y, main_rect.top);
+			current_position.y = max(current_position.y - step, main_rect.top);
 			break;
 		case VK_DOWN:
-			if (ctrl)
-				current_position.y += step.fast;
-			current_position.y += step.slow;
-			current_position.y = min(current_position.y, main_rect.bottom - SHIP_LENGTH);
+			current_position.y = min(current_position.y + step, main_rect.bottom - SHIP_LENGTH);
 			break;
 		case VK_LEFT:
-			if (ctrl)
-				current_position.x -= step.fast;
-			current_position.x -= step.slow;
-			current_position.x = max(current_position.x, main_rect.left);
+			current_position.x = max(current_position.x - step, main_rect.left);
 			break;
 		case VK_RIGHT:
-			if (ctrl)
-				current_position.x += step.fast;
-			current_position.x += step.slow;
-			current_position.x = min(current_position.x, main_rect.right - SHIP_LENGTH);
+			current_position.x = min(current_position.x + step, main_rect.right - SHIP_LENGTH);
 			break;
+		default:
+			return;
 		}
 
-		if (vk == VK_UP || vk == VK_DOWN || vk == VK_LEFT || vk == VK_RIGHT) {
-			ship.isMoving = true;
-			DWORD current_style = GetWindowLong(ship, GWL_STYLE);
-			SetWindowLong(ship, GWL_STYLE, WS_BORDER | current_style);
-			SetWindowPos(ship, *this, current_position.x, current_position.y, 0, 0,
-				SWP_FRAMECHANGED | SWP_NOSIZE | SWP_NOZORDER);
-		}
+		DWORD current_style = GetWindowLong(ship, GWL_STYLE);
+		SetWindowLong(ship, GWL_STYLE, WS_BORDER | current_style);
+		SetWindowPos(ship, *this, current_position.x, current_position.y, 0, 0,
+			SWP_FRAMECHANGED | SWP_NOSIZE | SWP_NOZORDER);
+
 	}
 	void OnDestroy() {
 		::PostQuitMessage(0);
