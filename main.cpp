271e20ec56c36183b09e70a46e77ccc5c2ae1d9a
Hrvoje Kuhar <hrkuhar@vsite.hr>
2018-11-04 14:56:31 +0100

    added a controllable ship to main window

--- main.cpp
+++ main.cpp
@@ -1,24 +1,78 @@
 #include "nwpwin.h"
 
-// TODO: prepare class (Static) for a ship
+class Static : public Window
+{
+public:
+	std::string ClassName() override { return "STATIC"; }
+};
 
 class MainWindow : public Window
 {
 protected:
 	void OnLButtonDown(POINT p) {
-		// TODO: create ship if it doesn't exist yet
-		// TODO: change current location
+		current_position = p;
+
+		if (!ship)
+		{
+			ship.Create(*this, WS_CHILD | WS_VISIBLE | SS_CENTER, "X", 1, current_position.x, current_position.y, 20, 20);
+		}
+		else {
+			::SetWindowPos(ship, 0, current_position.x, current_position.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+		}
 	}
 	void OnKeyUp(int vk) {
-		// TODO: mark ship (if exists) as "not moving"
+		if (ship) {
+			SetWindowLong(ship, GWL_STYLE, WS_CHILD | WS_VISIBLE | SS_CENTER);
+			SetWindowPos(ship, NULL, current_position.x, current_position.y, 0, 0, SWP_FRAMECHANGED | SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER);
+		}
 	}
 	void OnKeyDown(int vk) {
-		// TODO: if ship exists, move it depending on key and mark as "moving"
+		if (ship) {
+
+			int speed = GetKeyState(VK_CONTROL) < 0 ? 20 : 5;
+
+			RECT window_rect;
+			GetClientRect(*this, &window_rect);
+
+			RECT ship_rect;
+			GetClientRect(ship, &ship_rect);
+
+			switch (vk) {
+			case VK_UP:
+				if (current_position.y - speed >= window_rect.top)
+				{
+					current_position.y -= speed;
+				}
+				break;
+			case VK_DOWN: 
+				if (current_position.y + ship_rect.bottom + speed <= window_rect.bottom)
+				{
+					current_position.y += speed;
+				}
+				break;
+			case VK_LEFT: 
+				if (current_position.x - speed >= window_rect.left)
+				{
+					current_position.x -= speed;
+				}
+				break;
+			case VK_RIGHT: 
+				if (current_position.x + ship_rect.right + speed <= window_rect.right)
+				{
+					current_position.x += speed;
+				}
+				break;
+			}
+			SetWindowLong(ship, GWL_STYLE, WS_CHILD | WS_VISIBLE | SS_CENTER | WS_BORDER);
+			SetWindowPos(ship, 0, current_position.x, current_position.y, 0, 0, SWP_NOSIZE | SWP_FRAMECHANGED | SWP_NOZORDER);
+		}
 	}
 	void OnDestroy(){
 		::PostQuitMessage(0);
 	}
 private:
+	Static ship;
+	POINT current_position;
 };
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
