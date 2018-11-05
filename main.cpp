08d2de936f4c8e6099500628a249a26cfecf0980
Luka Radenovic <luka@artkod.com>
2018-11-05 20:44:07 +0100

    Dovrsena vjezba 4

--- main.cpp
+++ main.cpp
@@ -1,24 +1,53 @@
 #include "nwpwin.h"
 
-// TODO: prepare class (Static) for a ship
+class Static : public Window {
+protected:
+	std::string ClassName() override { return "STATIC"; };
+};
 
 class MainWindow : public Window
 {
+private:
+	Static ship;
+	POINT current_position;
 protected:
 	void OnLButtonDown(POINT p) {
-		// TODO: create ship if it doesn't exist yet
-		// TODO: change current location
+		current_position = p;
+		if (!ship)
+			ship.Create(*this, WS_CHILD | WS_VISIBLE | SS_CENTER | BS_CENTER, "X", 0, p.x, p.y, 30, 30);
+		
+		SetWindowPos(ship, NULL, p.x, p.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+		current_position = p;
 	}
 	void OnKeyUp(int vk) {
-		// TODO: mark ship (if exists) as "not moving"
+		if (ship) {
+			SetWindowLong(ship, GWL_STYLE, WS_VISIBLE | WS_CHILD);
+			SetWindowPos(ship, NULL, current_position.x, current_position.y, 0, 0, SWP_NOSIZE | SWP_FRAMECHANGED | SWP_NOZORDER | SWP_NOMOVE);
+		}
 	}
 	void OnKeyDown(int vk) {
-		// TODO: if ship exists, move it depending on key and mark as "moving"
+		int steps = ::GetAsyncKeyState(VK_CONTROL) ? 20 : 5;
+		
+		RECT window_rect;
+		GetClientRect(*this, &window_rect);
+
+		switch (vk) {
+			case VK_UP: 
+				current_position.y = max(0, current_position.y - steps); break;
+			case VK_DOWN: 
+				current_position.y = min(window_rect.bottom - 30, current_position.y + steps); break;
+			case VK_LEFT: 
+				current_position.x = max(0, current_position.x - steps); break;
+			case VK_RIGHT: 
+				current_position.x = min(window_rect.right - 30, current_position.x + steps); break;
+			default: return;
+		}
+		SetWindowLong(ship, GWL_STYLE, WS_CHILD | WS_VISIBLE | WS_BORDER);
+		SetWindowPos(ship, 0, current_position.x, current_position.y, 0, 0, SWP_NOZORDER | SWP_NOSIZE | SWP_FRAMECHANGED);
 	}
-	void OnDestroy(){
+	void OnDestroy() {
 		::PostQuitMessage(0);
 	}
-private:
 };
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
