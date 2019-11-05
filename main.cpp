14c089213d0393e65b5112ab44064b09750605de
Dona Pejnović <don@DESKTOP-QROSDFJ>
2019-11-05 15:22:20 +0100

    brodić vj4 -brodić se klikom postavlja na određeno mjesto -dodana funkcionalnost pomicanja -brodić se može pomoću ctrl tipke kretati brže -kada brodić mjenja poziciju pojavljuje se obrub

--- main.cpp
+++ main.cpp
@@ -1,24 +1,60 @@
 #include "nwpwin.h"
 
-// TODO: prepare class (Static) for a ship
+class Static :public Window {
+public:
+	std::string ClassName() override { return "STATIC"; }
+};
 
 class MainWindow : public Window
 {
 protected:
 	void OnLButtonDown(POINT p) {
-		// TODO: create ship if it doesn't exist yet
-		// TODO: change current location
+		
+		if (!ship)
+			ship.Create(*this, current_style, "X", 0, p.x, p.y, 20, 20);
+
+		
+		::SetWindowPos(ship, 0, p.x, p.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+		current_position = p;
 	}
 	void OnKeyUp(int vk) {
-		// TODO: mark ship (if exists) as "not moving"
+		
+		::SetWindowLong(ship, GWL_STYLE, current_style);
+		::SetWindowPos(ship, 0, current_position.x, current_position.y, 0, 0, SWP_FRAMECHANGED | SWP_NOSIZE | SWP_NOMOVE | SWP_NOZORDER);
+		
 	}
 	void OnKeyDown(int vk) {
-		// TODO: if ship exists, move it depending on key and mark as "moving"
+		
+		int steps = GetAsyncKeyState(VK_CONTROL) ? 10 : 5;
+		RECT rc;
+		::GetClientRect(*this, &rc);
+		switch (vk) {
+		case VK_UP:
+			current_position.y = max(current_position.y - steps, rc.top);
+			break;
+		case VK_DOWN:
+			current_position.y = min(current_position.y + steps, rc.bottom-20);
+			break;
+		case VK_LEFT:
+			current_position.x = max(current_position.x - steps, rc.left);
+			break;
+		case VK_RIGHT:
+			current_position.x = min(current_position.x + steps, rc.right - 20);
+			break;
+		default:
+			return;
+		}
+		::SetWindowLong(ship, GWL_STYLE, current_style | WS_BORDER);
+		::SetWindowPos(ship, 0, current_position.x, current_position.y, 0, 0, SWP_FRAMECHANGED | SWP_NOSIZE | SWP_NOZORDER);
 	}
 	void OnDestroy(){
 		::PostQuitMessage(0);
 	}
 private:
+	Static ship;
+	POINT current_position;
+	CONST DWORD current_style = WS_CHILD | WS_VISIBLE | SS_CENTER;
+
 };
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
