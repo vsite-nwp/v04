30908a8b354df77bd525432b6217f5c833666739
Igor Obradovic <iobradovic@ZAWMIOB>
2019-11-06 14:24:00 +0100

    Popravljanje gresaka.

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ protected:
 	void OnLButtonDown(POINT p) {
 		if (!mShip)
 		{
-			mShip.Create(*this, WS_VISIBLE|WS_CHILD | WS_BORDER, "X", 1, p.x, p.y, mWidth, mHeight);
+			mShip.Create(*this, mStyle, "X", 1, p.x, p.y, mWidth, mHeight);
 		}
 		mPosition = p;
 		::SetWindowPos(mShip, nullptr, p.x, p.y, NULL, NULL, SWP_NOSIZE | SWP_NOZORDER);
@@ -29,14 +29,16 @@ protected:
 		if (mShip)
 		{
 			::SetWindowLong(mShip, GWL_STYLE, GetWindowLong(mShip, GWL_STYLE) & ~WS_BORDER);
-			::SetWindowPos(mShip, nullptr, mPosition.x, mPosition.y, NULL, NULL, SWP_FRAMECHANGED | SWP_NOZORDER | SWP_NOSIZE | SWP_NOZORDER);
+			::SetWindowPos(mShip, nullptr, mPosition.x, mPosition.y, NULL, NULL, SWP_NOSIZE | SWP_FRAMECHANGED | SWP_NOZORDER | SWP_NOMOVE);
 		}
+		
 
 	}
 	void OnKeyDown(int vk) {
 		if (mShip) 
 		{
-			bool boost= IsKeyPressedAsync(VK_CONTROL);
+			mIsMoving = false;
+			bool boost= ::GetAsyncKeyState(VK_CONTROL);
 			int speed = boost ? 10 : 5;
 			int offsetX = 0;
 			int offsetY = 0;
@@ -44,60 +46,70 @@ protected:
 			{
 				case VK_UP:
 				{
-					offsetX = IsKeyPressedAsync(VK_LEFT) ?  -speed : 0;
-					offsetX = IsKeyPressedAsync(VK_RIGHT) ? speed : 0;			
+					if (CheckState(VK_LEFT)) offsetX = -speed;
+					if (CheckState(VK_RIGHT)) offsetX = speed;
+
 					offsetY = -speed;
 					Move(offsetX,offsetY);
 					break;
 				}	
 				case VK_DOWN:
 				{
-					offsetX = IsKeyPressedAsync(VK_LEFT) ? -speed : 0;
-					offsetX = IsKeyPressedAsync(VK_RIGHT) ? speed : 0;
+					if (CheckState(VK_LEFT)) offsetX = -speed;
+					if (CheckState(VK_RIGHT)) offsetX = speed;
+	
+					
 					offsetY = speed;
 					Move(offsetX, offsetY);
 					break;
 				}
 				case VK_LEFT:
 				{
-					offsetY = IsKeyPressedAsync(VK_UP) ? -speed : 0;
-					offsetY = IsKeyPressedAsync(VK_DOWN) ? speed : 0;
+					if (CheckState(VK_UP)) offsetY = -speed;
+					if (CheckState(VK_DOWN)) offsetY = speed;
+				
 					offsetX = -speed;
 					Move(offsetX, offsetY);
 					break;
 				}
 				case VK_RIGHT:
 				{
-					offsetY = IsKeyPressedAsync(VK_UP) ? -speed : 0;
-					offsetY = IsKeyPressedAsync(VK_DOWN) ? speed : 0;
+					if (CheckState(VK_UP)) offsetY = -speed;
+					if (CheckState(VK_DOWN)) offsetY = speed;
+
 					offsetX = speed;
 					Move(offsetX, offsetY);
 					break;
 				}
 			}
-			::SetWindowLong(mShip, GWL_STYLE, GetWindowLong(mShip, GWL_STYLE) | WS_BORDER);
+			if (mIsMoving)
+			{
+				::SetWindowLong(mShip, GWL_STYLE, mStyle | WS_BORDER);
+			}
 			::SetWindowPos(mShip, nullptr, mPosition.x, mPosition.y, NULL, NULL, SWP_NOSIZE | SWP_FRAMECHANGED | SWP_NOZORDER);
 		}
 	}
-	bool IsKeyPressedAsync(int key)
+	bool CheckState(const int& vk)
 	{
-		// Test high bit - if set, key was down when GetAsyncKeyState was called. 
-		return ((1 << 15) & ::GetAsyncKeyState(key)) != 0;
+
+		return (::GetKeyState(vk) & (1 << 15));
 	}
 	void Move(const int& offsetX, const int& offsetY)
 	{
 		RECT rect;
 		::GetClientRect(*this, &rect);
+		mIsMoving = true;
 		if (offsetX > 0 && mPosition.x + mWidth <= rect.right)
 		{
 			mPosition.x += offsetX;
-			
+
 		}
-		else if(mPosition.x > 0 && offsetX < 0)
+		else if (mPosition.x > 0 && offsetX < 0)
 		{
 			mPosition.x += offsetX;
-			
+
 		}
+
 		if (offsetY > 0 && mPosition.y + mHeight <= rect.bottom)
 		{
 			mPosition.y += offsetY;
@@ -114,7 +126,8 @@ private:
 	const unsigned int mWidth, mHeight;
 	Static mShip;
 	POINT  mPosition;
-
+	const DWORD mStyle = WS_CHILD | WS_VISIBLE | SS_CENTER;
+	bool mIsMoving;
 };
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
