98c33bc4c03b70720acd04964124dae8dd122a3c
Sara Cuzele <saracuzele@gmail.com>
2019-11-10 13:43:14 +0100

    v04 - moving the ship

--- main.cpp
+++ main.cpp
@@ -1,30 +1,80 @@
 #include "nwpwin.h"
 
-// TODO: prepare class (Static) for a ship
-
+class Static :public Window {
+public:
+	std::string ClassName() {
+		return "STATIC";
+	}
+};
 class MainWindow : public Window
 {
+private:
+	Static ship;
+	POINT currentPos;
+	bool isMoving;
+
 protected:
 	void OnLButtonDown(POINT p) {
-		// TODO: create ship if it doesn't exist yet
-		// TODO: change current location
+		if (!ship) {
+			ship.Create(*this, WS_CHILD | WS_VISIBLE | SS_CENTER, "X", NULL, p.x, p.y, 20, 20);
+		}
+		currentPos = p;
+		isMoving = false;
+		move();
 	}
+
 	void OnKeyUp(int vk) {
-		// TODO: mark ship (if exists) as "not moving"
+		if (!ship)
+			return;
+		isMoving = false;
+		move();
 	}
+
 	void OnKeyDown(int vk) {
-		// TODO: if ship exists, move it depending on key and mark as "moving"
+
+		if (ship)
+		{
+			int speed = GetKeyState(VK_CONTROL) < 0 ? 30 : 15;
+			RECT rect;
+			GetClientRect(*this, &rect);
+
+			switch (vk) {
+				case (VK_LEFT):
+					currentPos.x = max(0, currentPos.x - speed);
+					break;
+				case (VK_RIGHT):
+					currentPos.x = min(rect.right - 20, currentPos.x + speed);
+					break;
+				case (VK_UP):
+					currentPos.y = max(0, currentPos.y - speed);
+					break;
+				case(VK_DOWN):
+					currentPos.y = min(rect.bottom - 20, currentPos.y + speed);
+					break;
+				default:
+					return;
+			}
+			isMoving = true;
+			move();
+		}
 	}
+
 	void OnDestroy(){
 		::PostQuitMessage(0);
 	}
-private:
+
+	void move() {
+		DWORD style = ::GetWindowLong(ship, GWL_STYLE);
+		style = isMoving ? (style | WS_BORDER) : (style & ~WS_BORDER);
+		SetWindowLong(ship, GWL_STYLE, style);
+		SetWindowPos(ship, NULL, currentPos.x, currentPos.y, 0, 0, SWP_FRAMECHANGED | SWP_NOSIZE | SWP_NOZORDER);
+	}
 };
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 {
 	Application app;
 	MainWindow wnd;
-	wnd.Create(NULL, WS_OVERLAPPEDWINDOW | WS_VISIBLE, "NWP 4");
+	wnd.Create(NULL, WS_OVERLAPPEDWINDOW | WS_VISIBLE, "move the ship!!");
 	return app.Run();
 }
