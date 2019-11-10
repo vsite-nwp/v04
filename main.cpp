999e2f75c3cb1f793486e023f7a584bd484c33ee
Marko Boroš <Marko.Boros@vsite.hr>
2019-11-10 16:58:08 +0100

    Gotova vježba 4

--- main.cpp
+++ main.cpp
@@ -1,24 +1,118 @@
 #include "nwpwin.h"
 
-// TODO: prepare class (Static) for a ship
+class Static :public Window {
+	std::string ClassName()override
+	{
+		return "static";
+	}
 
+};
 class MainWindow : public Window
 {
+	Static shippy;
+	POINT po;
+	RECT size;
+	int step = 3;
+	bool movestatus;
+	void setStatus(bool isMoving) {
+		if (isMoving)
+		{
+			SetWindowLong(shippy, GWL_STYLE, GetWindowLong(shippy, GWL_STYLE) | WS_BORDER);
+		}
+		else
+		{
+			SetWindowLong(shippy, GWL_STYLE, GetWindowLong(shippy, GWL_STYLE) & ~WS_BORDER);
+		}
+	}
+	void teleport() {
+		setStatus(movestatus);
+		SetWindowPos(shippy, 0, po.x, po.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
+	}
+	bool checkForOutside(POINT coords, RECT size) {
+		if (coords.x >= size.right - 20 || coords.y >= size.bottom - 20 || coords.x <= size.left || coords.y <= size.top) {
+			return false;
+		}
+		return true;
+	}
 protected:
 	void OnLButtonDown(POINT p) {
-		// TODO: create ship if it doesn't exist yet
-		// TODO: change current location
+		GetClientRect(*this, &size);
+		po.x = p.x;
+		po.y = p.y;
+		if (checkForOutside(p, size)) {
+			if (!shippy)
+			{
+				shippy.Create(*this, WS_CHILD | WS_VISIBLE | SS_CENTER, "X", 0, p.x, p.y, 20, 20);
+				movestatus = false;
+			}
+			teleport();
+		}
 	}
 	void OnKeyUp(int vk) {
-		// TODO: mark ship (if exists) as "not moving"
+		movestatus = false;
+		teleport();
 	}
 	void OnKeyDown(int vk) {
-		// TODO: if ship exists, move it depending on key and mark as "moving"
+		if (GetKeyState(VK_CONTROL)<0) {
+			step = 12;
+		}
+		else
+		{
+			step = 4;
+		}
+		movestatus = true;
+		switch (vk)
+		{
+		case VK_LEFT:
+		{
+			po.x -= step;
+			if (!checkForOutside(po, size))
+			{
+				po.x += step;
+				movestatus = false;
+			}
+			break;
+		}
+		case VK_RIGHT:
+		{
+			po.x += step;
+			if (!checkForOutside(po, size))
+			{
+				po.x -= step;
+				movestatus = false;
+			}
+			break;
+		}
+		case VK_DOWN:
+		{
+			po.y += step;
+			if (!checkForOutside(po, size))
+			{
+				po.y -= step;
+				movestatus = false;
+			}
+			break;
+		}
+		case VK_UP:
+		{
+			po.y -= step;
+			if (!checkForOutside(po, size))
+			{
+				po.y += step;
+				movestatus = false;
+			}
+			break;
+		}
+		default:
+			break;
+		}
+		teleport();
 	}
-	void OnDestroy(){
+	void OnDestroy() {
 		::PostQuitMessage(0);
 	}
 private:
+
 };
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
