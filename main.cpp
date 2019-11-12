66a99dfd838435af3c4ca18413fe39c73f132cea
Auro Mazzi <auro.mazzi@gmail.com>
2019-11-13 00:50:24 +0100

    Done

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include "nwpwin.h"
+#define SHIPSIZE 20
+#define SLOW 10
+#define FAST 30
+#define BSTYLE (WS_CHILD | WS_VISIBLE | WS_BORDER | SS_CENTER)
 
 class Static : public Window
 {
 public:
-	std::string ClassName() { return "STATIC"; }
+	std::string ClassName() 
+	{ 
+		return "STATIC"; 
+	}
 };
-
 
 class MainWindow : public Window
 {
@@ -14,21 +20,50 @@ protected:
 
 		if (!Ship)
 		{
-		Ship.Create(*this, WS_CHILD | WS_VISIBLE | SS_CENTER, "x", 0, p.x, p.y, 20, 20);
+		Ship.Create(*this, BSTYLE & ~WS_BORDER, "x", 0, p.x, p.y, SHIPSIZE, SHIPSIZE);
 		}
-		SetWindowPos(Ship, NULL, p.x, p.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+		SetWindowPos(Ship, 0, p.x, p.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+		position = p;
 	}
 	void OnKeyUp(int vk) {
-		// TODO: mark ship (if exists) as "not moving"
+		if (Ship) {
+			SetWindowLong(Ship, GWL_STYLE, BSTYLE & ~WS_BORDER);
+			SetWindowPos(Ship, 0, position.x, position.y, 0, 0, SWP_FRAMECHANGED | SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER);
+		}
 	}
 	void OnKeyDown(int vk) {
-		// TODO: if ship exists, move it depending on key and mark as "moving"
+		if(Ship)
+		{
+			RECT sea;
+			GetClientRect(*this, &sea);
+			int move = GetAsyncKeyState(VK_CONTROL) ? FAST : SLOW;
+			switch(vk)
+			{
+				case VK_DOWN:
+					position.y = min(sea.bottom - SHIPSIZE, position.y + move);
+					break;
+				case VK_UP:
+					position.y = max(sea.top, position.y - move);
+					break;
+				case VK_RIGHT:
+					position.x = min(sea.right - SHIPSIZE, position.x + move);
+					break;
+				case VK_LEFT:
+					position.x = max(sea.left, position.x - move);
+					break;
+				default:
+					return;
+			}
+			SetWindowLong(Ship, GWL_STYLE, BSTYLE);
+			SetWindowPos(Ship, 0, position.x, position.y, 0, 0, SWP_FRAMECHANGED | SWP_NOSIZE | SWP_NOZORDER);
+		}		
 	}
 	void OnDestroy(){
 		::PostQuitMessage(0);
 	}
 private:
 	Static Ship;
+	POINT position;
 };
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
