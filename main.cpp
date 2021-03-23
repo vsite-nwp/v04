f2737a0824df0251e0351a45893c5d5eee2c529e
Topesa <pesa710@gmail.com>
2021-03-23 12:01:02 +0100

    Implement on_key_down

--- main.cpp
+++ main.cpp
@@ -4,16 +4,14 @@ class Static : public vsite::nwp::window
 {
 public:
 	std::string class_name() override { return "Static"; }
-
 };
 
 class main_window : public vsite::nwp::window
 {
 private:
 	Static ship;
-	POINT point;
+	POINT position;
 	DWORD style = WS_CHILD | WS_VISIBLE | SS_CENTER;
-
 
 protected:
 	void on_left_button_down(POINT p) override { 
@@ -24,19 +22,43 @@ protected:
 		}
 		
 		SetWindowPos(ship, 0, p.x, p.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
-		point = p;
+		position = p;
 	}
 	void on_key_up(int vk) override {
 		
 		if (ship)
 		{
 			SetWindowLong(ship, GWL_STYLE, style);
-			SetWindowPos(ship, NULL, NULL, NULL, 0, 0, SWP_NOSIZE | SWP_FRAMECHANGED | SWP_NOMOVE | SWP_NOZORDER);
+			SetWindowPos(ship, 0, 0, 0, 0, 0, SWP_NOSIZE | SWP_FRAMECHANGED | SWP_NOMOVE | SWP_NOZORDER);
 		}
-
 	}
 	void on_key_down(int vk) override {
-		// TODO: if ship exists, move it depending on key and mark as "moving"
+
+		int ctrl = GetAsyncKeyState(VK_CONTROL) ? 10 : 5;
+		RECT rect;
+		GetClientRect(*this, &rect);
+
+		if (ship)
+		{
+			switch (vk)
+			{
+			case VK_LEFT:
+				position.x = max(position.x - ctrl, rect.left);
+				break;
+			case VK_UP:
+				position.y = max(position.y - ctrl, rect.top);
+				break;
+			case VK_RIGHT:
+				position.x = min(position.x + ctrl, rect.right - 30); 
+				break;
+			case VK_DOWN:
+				position.y = min(position.y + ctrl, rect.bottom - 20);
+				break;
+			}
+		}
+
+		SetWindowLong(ship, GWL_STYLE, style | WS_BORDER);
+		SetWindowPos(ship, 0, position.x, position.y, 0, 0, SWP_FRAMECHANGED | SWP_NOSIZE | SWP_NOZORDER);
 	}
 	void on_destroy() override {
 		::PostQuitMessage(0);
