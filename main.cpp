121a11e58ac1a45b29acd6e25386cf32a44c6aa8
ivana andelic <54909467+ivana474@users.noreply.github.com>
2021-03-24 11:43:30 +0100

    verzija koja ne radi

--- main.cpp
+++ main.cpp
@@ -1,13 +1,25 @@
 #include "nwpwin.h"
 
 // TODO: prepare class ("STATIC") for a ship
+class ship :public vsite::nwp::window {
+	virtual std::string class_name() { return "STATIC"; }
+
+
+};
 
 class main_window : public vsite::nwp::window
 {
+	ship brod;
+	POINT position;
 protected:
 	void on_left_button_down(POINT p) override { 
 		// TODO: create ship if it doesn't exist yet
+		if(!brod)
+		brod.create(*this, WS_CHILD | WS_VISIBLE | WS_BORDER, "X", 1, p.x,p.y,50,50);
+		position = p;
+		
 		// TODO: change current location
+		SetWindowPos(brod, 0, p.x, p.y, 0, 0, SWP_NOZORDER | SWP_NOSIZE);
 	}
 	void on_key_up(int vk) override {
 		// TODO: mark ship (if exists) as "not moving"
