95063359c75cda18506e7e76d91548ddeb2a7a0f
Jasmin Husić <jhsc1988@gmail.com>
2021-03-25 16:24:08 +0100

    done

--- main.cpp
+++ main.cpp
@@ -1,24 +1,56 @@
 #include "nwpwin.h"
+using namespace vsite::nwp;
 
-// TODO: prepare class ("STATIC") for a ship
+class STATIC : public window {
+public:
+	std::string class_name() override { return "STATIC"; }
+};
 
 class main_window : public vsite::nwp::window
 {
 protected:
-	void on_left_button_down(POINT p) override { 
-		// TODO: create ship if it doesn't exist yet
-		// TODO: change current location
+	void on_left_button_down(POINT p) override {
+		if (!ship) {
+			ship.create(*this, WS_CHILD | WS_VISIBLE | SS_CENTER, "x", 0, p.x, p.y, 20, 20);
+		}
+		else
+		{
+			SetWindowPos(ship, NULL, p.x, p.y, 0, 0, SWP_NOZORDER | SWP_NOSIZE);
+		}
+		position = p;
 	}
 	void on_key_up(int vk) override {
-		// TODO: mark ship (if exists) as "not moving"
+		SetWindowLong(ship, GWL_STYLE, WS_CHILD | WS_VISIBLE | SS_CENTER);
+		SetWindowPos(ship, NULL, 0, 0, 0, 0, SWP_NOZORDER | SWP_NOSIZE | SWP_NOMOVE);
 	}
 	void on_key_down(int vk) override {
-		// TODO: if ship exists, move it depending on key and mark as "moving"
+
+		RECT r; GetClientRect(*this, &r);
+		int s = GetKeyState(VK_CONTROL) & WM_KEYDOWN ? 100 : 10;
+
+		switch (vk) {
+		case VK_UP:
+			position.y = max(position.y - 1 * s, r.top);
+			break;
+		case VK_DOWN:
+			position.y = min(position.y + 1 * s, r.bottom - 20);
+			break;
+		case VK_LEFT:
+			position.x = max(position.x - 1 * s, r.left);
+			break;
+		case VK_RIGHT:
+			position.x = min(position.x + 1 * s, r.right - 20);
+			break;
+		}
+		SetWindowLong(ship, GWL_STYLE, WS_CHILD | WS_VISIBLE | SS_CENTER | WS_BORDER);
+		SetWindowPos(ship, 0, position.x, position.y, 0, 0, SWP_FRAMECHANGED | SWP_NOSIZE | SWP_NOZORDER);
 	}
 	void on_destroy() override {
 		::PostQuitMessage(0);
 	}
 private:
+	STATIC ship;
+	POINT position;
 };
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
