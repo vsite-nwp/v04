f11e2007ab3ff69808c86a163cbc9ad8d8084121
Filip Lončar <filoncar@vsite.hr>
2022-04-11 19:54:02 +0200

    half done

--- main.cpp
+++ main.cpp
@@ -12,7 +12,6 @@ public:
 	string class_name() override {
 		return "static";
 	}
-
 };
 
 class main_window : public vsite::nwp::window
@@ -22,7 +21,7 @@ class main_window : public vsite::nwp::window
 protected:
 	void on_left_button_down(POINT p) override {   //WM_LBUTTONDOWN
 		// TODO: create ship if it doesn't exist yet
-		// TODO: change current location
+		
 
 		location = p;
 		
@@ -30,13 +29,62 @@ protected:
 			ship.create(*this, WS_VISIBLE | WS_CHILD | SS_CENTER, "X", 0, location.x, location.y, 20, 20);
 		}
 
+		// TODO: change current location
 		SetWindowPos(ship, 0, location.x, location.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
 	}
 	void on_key_up(int vk) override {  //WM_KEYUP
 		// TODO: mark ship (if exists) as "not moving"
+
+		DWORD style = GetWindowLong(ship, GWL_STYLE);
+
+		if (ship) {
+			SetWindowLong(ship, GWL_STYLE, style &~WS_BORDER);
+
+			SetWindowPos(ship, 0, location.x, location.y, 0, 0, SWP_FRAMECHANGED | SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER);
+		}
+
+		
 	}
 	void on_key_down(int vk) override {  //WM_KEYDOWN
 		// TODO: if ship exists, move it depending on key and mark as "moving"
+
+		RECT rc;
+		GetClientRect(*this, &rc);
+		short key_press = GetAsyncKeyState(VK_CONTROL);
+		int speed;
+		const int plusDownRight = 20;
+
+		if (key_press < 0)
+			speed = 30;
+		else
+			speed = 3;
+
+		switch (vk) {
+		case VK_UP:
+			if(location.y>0 && (location.y-speed)>=0)
+				location.y -= speed;
+			break;
+		case VK_DOWN:
+			if(location.y+ plusDownRight <rc.bottom && ((location.y + plusDownRight) - speed) <= rc.bottom)
+				location.y += speed;
+			break;
+		case VK_LEFT:
+			if(location.x>0 && (location.x - speed) >= 0)
+				location.x -= speed;
+			break;
+		case VK_RIGHT:
+			if(location.x+ plusDownRight <rc.right && ((location.x + plusDownRight) - speed)  <= rc.right)
+				location.x += speed;
+			break;
+		}
+		//default:
+		//	return location.x=0,location.y=0;
+
+		DWORD style = GetWindowLong(ship, GWL_STYLE);
+
+		SetWindowLong(ship, GWL_STYLE, style|WS_BORDER);
+		SetWindowPos(ship, 0, location.x, location.y, 0, 0, SWP_FRAMECHANGED | SWP_NOSIZE | SWP_NOZORDER);
+
 	}
 	void on_destroy() override {
 		::PostQuitMessage(0);
