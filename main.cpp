7cfb895db160900e2d412863d8f1320114585477
Filip Lončar <filoncar@vsite.hr>
2022-04-11 20:44:34 +0200

    comments removed

--- main.cpp
+++ main.cpp
@@ -2,8 +2,6 @@
 
 using namespace std;
 
-// TODO: prepare class ("STATIC") for a ship
-
 POINT location;
 
 class STATIC : public vsite::nwp::window {
@@ -19,8 +17,7 @@ class main_window : public vsite::nwp::window
 	STATIC ship;
 
 protected:
-	void on_left_button_down(POINT p) override {   //WM_LBUTTONDOWN
-		// TODO: create ship if it doesn't exist yet
+	void on_left_button_down(POINT p) override { 
 		
 
 		location = p;
@@ -29,11 +26,9 @@ protected:
 			ship.create(*this, WS_VISIBLE | WS_CHILD | SS_CENTER, "X", 0, location.x, location.y, 20, 20);
 		}
 
-		// TODO: change current location
 		SetWindowPos(ship, 0, location.x, location.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
 	}
-	void on_key_up(int vk) override {  //WM_KEYUP
-		// TODO: mark ship (if exists) as "not moving"
+	void on_key_up(int vk) override {
 
 		DWORD style = GetWindowLong(ship, GWL_STYLE);
 
@@ -43,10 +38,8 @@ protected:
 			SetWindowPos(ship, 0, location.x, location.y, 0, 0, SWP_FRAMECHANGED | SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER);
 		}
 
-		
 	}
-	void on_key_down(int vk) override {  //WM_KEYDOWN
-		// TODO: if ship exists, move it depending on key and mark as "moving"
+	void on_key_down(int vk) override {  
 
 		RECT rc;
 		GetClientRect(*this, &rc);
@@ -80,7 +73,6 @@ protected:
 		default:
 			return;
 		}
-		// location.x=0,location.y=0;
 
 		DWORD style = GetWindowLong(ship, GWL_STYLE);
 
@@ -101,40 +93,3 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
 	w.create(0, WS_OVERLAPPEDWINDOW | WS_VISIBLE, "NWP 4");
 	return app.run();
 }
-
-
-
-
-/*
-* POINT location;
-* Static ship;
-* 
-* void on:left_button(POINT p) overrirde{
-* location = p:
-* if(ship){
-* ship.create();
-* SetWindowPos(ship,.....);
-* 
-* void on_key_up(int vk)override{
-* DWORD style = getWindowsLong(ship, GWl_STYLE);  - krajeci polja da ne izletimo izvan
-* //remove WS_BORDER bit
-* SetWindowLong(ship, GWL_STYLE, style);
-* }
-* void on_key_down(int vk) override{
-* RECT rc; GetClientReact(&rc);
-* int speed = getAyncKeyStatic(VK_CTRL) ? ;
-* 
-* 
-* switch(vk){
-* case VK_LEFT: location x -= spedd;
-* case VK_RIGHT : local.x=;
-* case VK_UP : location.y=;
-* case VK_DOWN : location.y=;
-* default return;
-* 
-* DWORD style = GetWindowsLong(ship, GWL_STYLE);
-* //add WS_BORDER bit
-* SetWindowsLong(ship,GWL_STYLE, style);
-* SetWindowPos(ship,..,);//
-* 
-*/
