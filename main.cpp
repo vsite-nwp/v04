536793550c40ef62e566aec1b20db19f319b3951
LukaPetranovic <lupetran@vsite.hr>
2022-06-10 20:04:47 +0200

    Resolved v04

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include "nwpwin.h"
 
-// TODO: prepare class ("STATIC") for a ship
+// : prepare class ("STATIC") for a ship
+
+class STATIC : public vsite::nwp::window {
+public:
+	std::string class_name()override {
+		return "Static";
+	}
+};
 
 class main_window : public vsite::nwp::window
 {
@@ -8,6 +15,10 @@ protected:
 	void on_left_button_down(POINT p) override { 
 		// TODO: create ship if it doesn't exist yet
 		// TODO: change current location
+		ship_pos = p;
+		if (!ship) {
+			ship.create(*this, WS_CHILD | WS_VISIBLE | SS_CENTER, "X", 0, p.x, p.y, shipY, shipX);
+		}
 	}
 	void on_key_up(int vk) override {
 		// TODO: mark ship (if exists) as "not moving"
