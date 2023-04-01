11ba5e8610c41851a084f3eb36f0dd06247781ba
Mihael Vlainic <mihael.vlainic1@gmail.com>
2023-04-01 10:06:24 +0200

    V04

--- main.cpp
+++ main.cpp
@@ -1,19 +1,89 @@
 #include "nwpwin.h"
 
-// TODO: prepare class ("STATIC") for a ship
+class Static : public vsite::nwp::window {
+public:
+	std::string class_name()override {
+		return "Static";
+	}
+};
 
 class main_window : public vsite::nwp::window
 {
+public:
+	Static ship;
+	POINT position;
+	RECT windowsBorder;
+	int speed = 5;
+	bool isFaster = false;
+
 protected:
 	void on_left_button_down(POINT p) override { 
-		// TODO: create ship if it doesn't exist yet
-		// TODO: change current location
+		if (!ship) {
+			ship.create(*this, WS_CHILD | SS_CENTER | WS_VISIBLE, "X", 1, p.x, p.y, 20, 15);
+		}
+		position = p;
+
+		SetWindowPos(ship, 0, p.x, p.y, 0, 0, SWP_NOZORDER | SWP_NOSIZE);
+
 	}
 	void on_key_up(int vk) override {
-		// TODO: mark ship (if exists) as "not moving"
+		if (!ship) {return;}
+
+		SetWindowLong(ship, GWL_STYLE, WS_CHILD | SS_CENTER | WS_VISIBLE);
+		SetWindowPos(ship, 0, 0, 0, 0, 0,SWP_NOZORDER | SWP_NOMOVE | SWP_FRAMECHANGED | SWP_NOSIZE);
+
 	}
 	void on_key_down(int vk) override {
-		// TODO: if ship exists, move it depending on key and mark as "moving"
+		if (!ship) {return;}
+
+		GetClientRect(*this, &windowsBorder);
+
+		switch (vk)
+		{
+		case VK_CONTROL:
+			if (isFaster)
+			{
+				speed = 5;
+				isFaster = false;
+			}
+			else { speed = 20; isFaster = true; }
+			break;
+		case VK_UP:
+			if (windowsBorder.top < position.y - speed){position.y -= speed;}
+			break;
+		case VK_DOWN:
+			if (windowsBorder.bottom - 15 > position.y + speed) { position.y += speed; }
+			break;
+		case VK_LEFT:
+			if (windowsBorder.left < position.x - speed){position.x -= speed;}
+			break;
+		case VK_RIGHT:
+			if (windowsBorder.right -20 > position.x + speed){position.x += speed;}
+			break;
+		case 87:
+			// W
+			if (windowsBorder.top < position.y - speed){position.y -= speed;}
+			break;
+		case 83:
+			// S
+			if (windowsBorder.bottom - 15 > position.y + speed){position.y += speed;}
+			break;
+		case 65:
+			// A
+			if (windowsBorder.left < position.x - speed){position.x -= speed;}
+			break;
+		case 68:
+			// D
+			if (windowsBorder.right - 20 > position.x + speed){position.x += speed;}
+			break;
+		default: 
+			return;
+			break;
+		}
+
+		SetWindowLong(ship, GWL_STYLE, WS_CHILD | WS_VISIBLE | SS_CENTER | WS_BORDER);
+		SetWindowPos(ship, 0, position.x, position.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
+
 	}
 	void on_destroy() override {
 		::PostQuitMessage(0);
