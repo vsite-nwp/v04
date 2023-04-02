de54d21e71fe81fb4cfda4fce0be3b9f8da97b57
Ivor Novosel <ivnovosel@vsite.hr>
2023-04-02 22:51:11 +0200

    Ispravljeno prema komentarima

--- main.cpp
+++ main.cpp
@@ -15,58 +15,55 @@ protected:
 		// create ship if it doesn't exist yet
 		// change current location
 		if (!x) {
-			x.create(*this, WS_CHILD|WS_VISIBLE, "X", 1, p.x, p.y, 11, 17);
+			x.create(*this, WS_CHILD|WS_VISIBLE, "X", 1, p.x, p.y, width, height);
 		}
 		else {
 			SetWindowPos(x, 0, p.x, p.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
 		}
 		current_pos = p;
-		style = GetWindowLong(x, GWL_STYLE);
 	}
 	void on_key_up(int vk) override {
 		// mark ship (if exists) as "not moving"
 		if (!x) return;
-		SetWindowLong(x, GWL_STYLE, style);
-		SetWindowPos(x, 0, current_pos.x, current_pos.y, 0, 0, SWP_FRAMECHANGED | SWP_NOSIZE | SWP_NOZORDER);
-		speed = 1;
+		if ((GetWindowLong(x, GWL_STYLE) & WS_BORDER)) SetWindowLong(x, GWL_STYLE, GetWindowLong(x, GWL_STYLE) - WS_BORDER);
+		SetWindowPos(x, 0, current_pos.x, current_pos.y, 0, 0, SWP_FRAMECHANGED | SWP_NOSIZE | SWP_NOZORDER | SWP_NOMOVE);
 	}
 	void on_key_down(int vk) override {
 		// if ship exists, move it depending on key and mark as "moving"
 		RECT wndSize;
 		GetClientRect(*this, &wndSize);
-		if (GetKeyState(VK_CONTROL) < 0) speed = 2;
+		int move = GetKeyState(VK_CONTROL) < 0 ? 8 : 4;
+		if (!(GetWindowLong(x, GWL_STYLE) & WS_BORDER)) SetWindowLong(x, GWL_STYLE, GetWindowLong(x, GWL_STYLE) + WS_BORDER);
 
 		if (!x) return;
 		else {
 			switch (vk) {
 				case VK_UP:
-					if (wndSize.top < current_pos.y) current_pos.y -= move * speed;
+					current_pos.y = wndSize.top < current_pos.y - move ? current_pos.y - move : wndSize.top;
 					break;
 				case VK_DOWN:
-					if (wndSize.bottom > current_pos.y + 17) current_pos.y += move * speed;
+					current_pos.y = wndSize.bottom > current_pos.y + height + move ? current_pos.y + move : wndSize.bottom - height;
 					break;
 				case VK_LEFT:
-					if (wndSize.left < current_pos.x) current_pos.x -= move * speed;
+					current_pos.x = wndSize.left < current_pos.x - move ? current_pos.x - move : wndSize.left;
 					break;
 				case VK_RIGHT:
-					if (wndSize.right > current_pos.x + 11) current_pos.x += move * speed;
+					current_pos.x = wndSize.right > current_pos.x + width + move ? current_pos.x + move : wndSize.right - width;
 					break;
 				default:
 					return;
 			}
 		}
 		SetWindowPos(x, 0, current_pos.x, current_pos.y, 0, 0, SWP_FRAMECHANGED | SWP_NOSIZE | SWP_NOZORDER);
-		SetWindowLong(x, GWL_STYLE, style + WS_BORDER);
 	}
 	void on_destroy() override {
 		::PostQuitMessage(0);
 	}
 private:
 	ship x;
-	int move = 4;
-	int speed = 1;
 	POINT current_pos;
-	LONG style;
+	static const int width = 11;
+	static const int height = 17;
 };
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hp, LPSTR cmdLine, int nShow)
